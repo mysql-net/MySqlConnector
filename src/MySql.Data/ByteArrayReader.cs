d5b1b92438f5425fc67cbd15dfac7947e30d150e
Bradley Grainger <bgrainger@gmail.com>
2016-03-22 22:03:17 -0700

    Fix whitespace.

--- src/MySql.Data/ByteArrayReader.cs
+++ src/MySql.Data/ByteArrayReader.cs
@@ -2,10 +2,10 @@
 
 namespace MySql.Data
 {
-    internal sealed class ByteArrayReader
-    {
-	    public ByteArrayReader(byte[] buffer, int offset, int length)
-	    {
+	internal sealed class ByteArrayReader
+	{
+		public ByteArrayReader(byte[] buffer, int offset, int length)
+		{
 			if (buffer == null)
 				throw new ArgumentNullException(nameof(buffer));
 			if (offset < 0)
@@ -13,15 +13,15 @@ namespace MySql.Data
 			if (offset + length > buffer.Length)
 				throw new ArgumentOutOfRangeException(nameof(length));
 
-		    m_buffer = buffer;
+			m_buffer = buffer;
 			m_maxOffset = offset + length;
-		    m_offset = offset;
-	    }
+			m_offset = offset;
+		}
 
-	    public ByteArrayReader(ArraySegment<byte> arraySegment)
-		    : this(arraySegment.Array, arraySegment.Offset, arraySegment.Count)
-	    {
-	    }
+		public ByteArrayReader(ArraySegment<byte> arraySegment)
+			: this(arraySegment.Array, arraySegment.Offset, arraySegment.Count)
+		{
+		}
 
 		public byte ReadByte()
 		{
@@ -30,10 +30,10 @@ namespace MySql.Data
 		}
 
 		public void ReadByte(byte value)
-	    {
+		{
 			if (ReadByte() != value)
 				throw new FormatException($"Expected to read 0x{value:X2} but got 0x{m_buffer[m_offset]:X2}");
-	    }
+		}
 
 		public short ReadInt16()
 		{
@@ -52,12 +52,12 @@ namespace MySql.Data
 		}
 
 		public int ReadInt32()
-	    {
-		    VerifyRead(4);
-		    var result = BitConverter.ToInt32(m_buffer, m_offset);
-		    m_offset += 4;
-		    return result;
-	    }
+		{
+			VerifyRead(4);
+			var result = BitConverter.ToInt32(m_buffer, m_offset);
+			m_offset += 4;
+			return result;
+		}
 
 		public uint ReadUInt32()
 		{
@@ -93,32 +93,32 @@ namespace MySql.Data
 
 		// TODO: Span<byte>
 		public byte[] ReadNullTerminatedByteString()
-	    {
-		    int index = m_offset;
-		    while (index < m_maxOffset && m_buffer[index] != 0)
-			    index++;
+		{
+			int index = m_offset;
+			while (index < m_maxOffset && m_buffer[index] != 0)
+				index++;
 			if (index == m_maxOffset)
 				throw new FormatException($"Read past end of buffer looking for NUL.");
 			byte[] substring = new byte[index - m_offset];
-		    Array.Copy(m_buffer, m_offset, substring, 0, substring.Length);
-		    m_offset = index + 1;
-		    return substring;
-	    }
+			Array.Copy(m_buffer, m_offset, substring, 0, substring.Length);
+			m_offset = index + 1;
+			return substring;
+		}
 
-	    public byte[] ReadByteString(int length)
-	    {
-		    VerifyRead(length);
-		    var result = new byte[length];
-		    Array.Copy(m_buffer, m_offset, result, 0, result.Length);
-		    m_offset += length;
-		    return result;
-	    }
+		public byte[] ReadByteString(int length)
+		{
+			VerifyRead(length);
+			var result = new byte[length];
+			Array.Copy(m_buffer, m_offset, result, 0, result.Length);
+			m_offset += length;
+			return result;
+		}
 
-	    public ulong ReadLengthEncodedInteger()
-	    {
-		    byte encodedLength = m_buffer[m_offset++];
-		    switch (encodedLength)
-		    {
+		public ulong ReadLengthEncodedInteger()
+		{
+			byte encodedLength = m_buffer[m_offset++];
+			switch (encodedLength)
+			{
 			case 0xFC:
 				return ReadFixedLengthUInt32(2);
 			case 0xFD:
@@ -128,8 +128,8 @@ namespace MySql.Data
 			case 0xFF:
 				throw new FormatException("Length-encoded integer cannot have 0xFF prefix byte.");
 			default:
-			    return encodedLength;
-		    }
+				return encodedLength;
+			}
 		}
 
 		public ArraySegment<byte> ReadLengthEncodedByteString()
@@ -140,13 +140,13 @@ namespace MySql.Data
 			return result;
 		}
 
-		public int BytesRemaining => m_maxOffset - m_offset ;
+		public int BytesRemaining => m_maxOffset - m_offset;
 
-	    private void VerifyRead(int length)
-	    {
-		    if (m_offset + length > m_maxOffset)
+		private void VerifyRead(int length)
+		{
+			if (m_offset + length > m_maxOffset)
 				throw new InvalidOperationException("Read past end of buffer.");
-	    }
+		}
 
 		readonly byte[] m_buffer;
 		readonly int m_maxOffset;
