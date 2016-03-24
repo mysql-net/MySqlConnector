d6fe9a521f1eeee1de009e75f70e55ee0827d493
Bradley Grainger <bgrainger@gmail.com>
2016-03-24 09:31:01 -0700

    Add string DataTypes tests.
    Refactor duplicated test code.

--- tests/SideBySide.New/DataTypes.cs
+++ tests/SideBySide.New/DataTypes.cs
@@ -1,5 +1,16 @@
 ﻿using System;
+using System.Data.Common;
 using Xunit;
+
+// Baseline MySql.Data will throw SqlNullValueException, which is an exception type related to SQL Server:
+// "The exception that is thrown when the Value property of a System.Data.SqlTypes structure is set to null."
+// However, DbDataReader.GetString etc. are documented as throwing InvalidCastException: https://msdn.microsoft.com/en-us/library/system.data.common.dbdatareader.getstring.aspx
+// Additionally, that is what DbDataReader.GetFieldValue<T> throws. For consistency, we prefer InvalidCastException.
+#if BASELINE
+using GetValueWhenNullException = System.Data.SqlTypes.SqlNullValueException;
+#else
+using GetValueWhenNullException = System.InvalidCastException;
+#endif
 
 namespace SideBySide
 {
@@ -10,48 +21,56 @@ namespace SideBySide
 			m_database = database;
 		}
 
-		[Fact]
-		public void QueryInt32()
+		[Theory]
+		[InlineData("Int32", new object[] { null, 0, int.MinValue, int.MaxValue, 123456789 })]
+		public void QueryInt32(string column, object[] expected)
 		{
-			using (var cmd = m_database.Connection.CreateCommand())
-			{
-				cmd.CommandText = "select Int32 from datatypes.numbers order by rowid";
-				using (var reader = cmd.ExecuteReader())
-				{
-					Assert.True(reader.Read());
-					Assert.Equal(DBNull.Value, reader.GetValue(0));
-					Assert.True(reader.Read());
-					Assert.Equal(0, reader.GetValue(0));
-					Assert.True(reader.Read());
-					Assert.Equal(int.MinValue, reader.GetValue(0));
-					Assert.True(reader.Read());
-					Assert.Equal(int.MaxValue, reader.GetValue(0));
-					Assert.True(reader.Read());
-					Assert.Equal(123456789, reader.GetValue(0));
-					Assert.False(reader.Read());
-					Assert.False(reader.NextResult());
-				}
-			}
+			DoQuery("numbers", column, expected, reader => reader.GetInt32(0));
 		}
 
-		[Fact]
-		public void QueryUInt32()
+		[Theory]
+		[InlineData("UInt32", new object[] { null, 0u, uint.MinValue, uint.MaxValue, 123456789u })]
+		public void QueryUInt32(string column, object[] expected)
+		{
+			DoQuery<InvalidCastException>("numbers", column, expected, reader => reader.GetFieldValue<uint>(0));
+		}
+
+		[Theory]
+		[InlineData("utf8", new[] { null, "", "ASCII", "Ũńıċōđĕ" })]
+		[InlineData("latin1", new[] { null, "", "ASCII", "Lãtïñ" })]
+		[InlineData("cp1251", new[] { null, "", "ASCII", "АБВГабвг" })]
+		public void QueryString(string column, string[] expected)
+		{
+			DoQuery("strings", column, expected, reader => reader.GetString(0));
+		}
+
+		private void DoQuery(string table, string column, object[] expected, Func<DbDataReader, object> getValue)
+		{
+			DoQuery<GetValueWhenNullException>(table, column, expected, getValue);
+		}
+
+		private void DoQuery<TException>(string table, string column, object[] expected, Func<DbDataReader, object> getValue)
+			where TException : Exception
 		{
 			using (var cmd = m_database.Connection.CreateCommand())
 			{
-				cmd.CommandText = "select UInt32 from datatypes.numbers order by rowid";
+				cmd.CommandText = $"select {column} from datatypes.{table} order by rowid";
 				using (var reader = cmd.ExecuteReader())
 				{
-					Assert.True(reader.Read());
-					Assert.Equal(DBNull.Value, reader.GetValue(0));
-					Assert.True(reader.Read());
-					Assert.Equal(0u, reader.GetValue(0));
-					Assert.True(reader.Read());
-					Assert.Equal(uint.MinValue, reader.GetValue(0));
-					Assert.True(reader.Read());
-					Assert.Equal(uint.MaxValue, reader.GetValue(0));
-					Assert.True(reader.Read());
-					Assert.Equal(123456789u, reader.GetValue(0));
+					foreach (var value in expected)
+					{
+						Assert.True(reader.Read());
+						if (value == null)
+						{
+							Assert.Equal(DBNull.Value, reader.GetValue(0));
+							Assert.Throws<TException>(() => getValue(reader));
+						}
+						else
+						{
+							Assert.Equal(value, reader.GetValue(0));
+							Assert.Equal(value, getValue(reader));
+						}
+					}
 					Assert.False(reader.Read());
 					Assert.False(reader.NextResult());
 				}
--- tests/SideBySide.New/DataTypesFixture.cs
+++ tests/SideBySide.New/DataTypesFixture.cs
@@ -25,6 +25,20 @@ values
   (-2147483648, 0), # minimum
   (2147483647, 4294967295), # maximum
   (123456789, 123456789);
+
+create table datatypes.strings (
+  rowid integer not null primary key auto_increment,
+  utf8 varchar(100) character set 'utf8mb4' null,
+  latin1 varchar(100) character set 'latin1' null,
+  cp1251 varchar(100) character set 'cp1251' null
+);
+
+insert into datatypes.strings(utf8, latin1, cp1251)
+values
+  (null, null, null),
+  ('', '', ''),
+  ('ASCII', 'ASCII', 'ASCII'),
+  ('Ũńıċōđĕ', 'Lãtïñ', 'АБВГабвг');
 ");
 	    }
 
