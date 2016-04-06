6c937842b8d3631b2365fd078651d796aa7e1e7a
Bradley Grainger <bgrainger@gmail.com>
2016-04-05 23:01:59 -0700

    Delete unused code.

--- src/MySql.Data/Serialization/SerializationUtility.cs
+++ src/MySql.Data/Serialization/SerializationUtility.cs
@@ -1,33 +1,7 @@
-using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-
 namespace MySql.Data.Serialization
 {
 	internal static class SerializationUtility
 	{
-		public static async Task<int> ReadAvailableAsync(this Stream stream, byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-		{
-			int totalBytesRead = 0;
-			while (count > 0)
-			{
-				int bytesRead = await stream.ReadAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
-				if (bytesRead == 0)
-					break;
-				totalBytesRead += bytesRead;
-				offset += bytesRead;
-				count -= bytesRead;
-			}
-			return totalBytesRead;
-		}
-
-		public static async Task ReadExactlyAsync(this Stream stream, byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-		{
-			var bytesRead = await stream.ReadAvailableAsync(buffer, offset, count, cancellationToken).ConfigureAwait(false);
-			if (bytesRead != count)
-				throw new EndOfStreamException();
-		}
-
 		public static uint ReadUInt32(byte[] buffer, int offset, int count)
 		{
 			uint value = 0;
