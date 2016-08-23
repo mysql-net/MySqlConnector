3c88cbc0d9b71c52e83af83df8cc1b743cb1c838
Bradley Grainger <bgrainger@gmail.com>
2016-08-22 21:12:17 -0700

    Add DbProviderFactoryTests.

---
+++ tests/MySqlConnector.Tests/DbProviderFactoryTests.cs
@@ -0,0 +1,25 @@
+using MySql.Data.MySqlClient;
+using Xunit;
+
+namespace MySql.Data.Tests
+{
+	public class DbProviderFactoryTests
+	{
+		[Fact]
+		public void CreatesExpectedTypes()
+		{
+			Assert.IsType<MySqlConnection>(MySqlClientFactory.Instance.CreateConnection());
+			Assert.IsType<MySqlConnectionStringBuilder>(MySqlClientFactory.Instance.CreateConnectionStringBuilder());
+			Assert.IsType<MySqlCommand>(MySqlClientFactory.Instance.CreateCommand());
+			Assert.IsType<MySqlParameter>(MySqlClientFactory.Instance.CreateParameter());
+		}
+
+		[Fact]
+		public void Singleton()
+		{
+			var factory1 = MySqlClientFactory.Instance;
+			var factory2 = MySqlClientFactory.Instance;
+			Assert.True(object.ReferenceEquals(factory1, factory2));
+		}
+	}
+}
