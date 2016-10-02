ba52e02f972531b366b422599447b8657c8bc69a
Bradley Grainger <bgrainger@gmail.com>
2016-10-02 08:44:06 -0700

    Add custom UTF-8 encoder.

--- src/MySqlConnector/Utility.cs
+++ src/MySqlConnector/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Text;
 
 namespace MySql.Data
@@ -23,5 +24,41 @@ namespace MySql.Data
 
 		public static string GetString(this Encoding encoding, ArraySegment<byte> arraySegment)
 			=> encoding.GetString(arraySegment.Array, arraySegment.Offset, arraySegment.Count);
+
+		public static void WriteUtf8(this BinaryWriter writer, string value) =>
+			WriteUtf8(writer, value, 0, value.Length);
+
+		public static void WriteUtf8(this BinaryWriter writer, string value, int startIndex, int length)
+		{
+			var endIndex = startIndex + length;
+			while (startIndex < endIndex)
+			{
+				int codePoint = char.ConvertToUtf32(value, startIndex);
+				startIndex++;
+				if (codePoint < 0x80)
+				{
+					writer.Write((byte) codePoint);
+				}
+				else if (codePoint < 0x800)
+				{
+					writer.Write((byte) (0xC0 | ((codePoint >> 6) & 0x1F)));
+					writer.Write((byte) (0x80 | (codePoint & 0x3F)));
+				}
+				else if (codePoint < 0x10000)
+				{
+					writer.Write((byte) (0xE0 | ((codePoint >> 12) & 0x0F)));
+					writer.Write((byte) (0x80 | ((codePoint >> 6) & 0x3F)));
+					writer.Write((byte) (0x80 | (codePoint & 0x3F)));
+				}
+				else
+				{
+					writer.Write((byte) (0xF0 | ((codePoint >> 18) & 0x07)));
+					writer.Write((byte) (0x80 | ((codePoint >> 12) & 0x3F)));
+					writer.Write((byte) (0x80 | ((codePoint >> 6) & 0x3F)));
+					writer.Write((byte) (0x80 | (codePoint & 0x3F)));
+					startIndex++;
+				}
+			}
+		}
 	}
 }
