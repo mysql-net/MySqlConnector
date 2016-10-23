748ce85fced96e4eeacfc8903f2b16680651cdd8
Caleb Lloyd <caleblloyd@gmail.com>
2016-10-23 13:53:05 -0400

    Unix Domain Socket. fixes #118

---
+++ src/MySqlConnector/Serialization/ConnectionType.cs
@@ -0,0 +1,18 @@
+﻿namespace MySql.Data.Serialization
+{
+	/// <summary>
+	/// Specifies whether to perform synchronous or asynchronous I/O.
+	/// </summary>
+	internal enum ConnectionType
+	{
+		/// <summary>
+		/// Connection is a TCP connection.
+		/// </summary>
+		Tcp,
+
+		/// <summary>
+		/// Connection is a Unix Domain Socket.
+		/// </summary>
+		Unix,
+	}
+}
