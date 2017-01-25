e68adf6427026bc40ecb658eb049831bc99910ca
Caleb Lloyd <caleblloyd@gmail.com>
2017-01-25 01:24:30 -0500

    buffer reads. fixes #164

--- src/MySqlConnector/Protocol/Serialization/IByteHandler.cs
+++ src/MySqlConnector/Protocol/Serialization/IByteHandler.cs
@@ -8,13 +8,13 @@ namespace MySql.Data.Protocol.Serialization
 		/// <summary>
 		/// Reads data from this byte handler.
 		/// </summary>
-		/// <param name="count">The number of bytes to read.</param>
+		/// <param name="buffer">The buffer to read into.</param>
 		/// <param name="ioBehavior">The <see cref="IOBehavior"/> to use when reading data.</param>
-		/// <returns>An <see cref="ArraySegment{Byte}"/> containing the data that was read. This will contain at most <paramref name="count"/> bytes.
-		/// If not all the data was available, fewer than <paramref name="count"/> bytes may be returned. If reading failed, zero bytes will be returned. This
+		/// <returns>A <see cref="ValueTask{Int32}"/>. Number of bytes read.</returns>
+		/// If reading failed, zero bytes will be returned. This
 		/// <see cref="ArraySegment{Byte}"/> will be valid to read from until the next time <see cref="ReadBytesAsync"/> or
 		/// <see cref="WriteBytesAsync"/> is called.</returns>
-		ValueTask<ArraySegment<byte>> ReadBytesAsync(int count, IOBehavior ioBehavior);
+		ValueTask<int> ReadBytesAsync(ArraySegment<byte> buffer, IOBehavior ioBehavior);
 
 		/// <summary>
 		/// Writes data to this byte handler.
