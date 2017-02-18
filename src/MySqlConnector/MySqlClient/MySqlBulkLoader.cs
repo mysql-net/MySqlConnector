69aee2086b3c5b7cd4e25a69b7e235a1f23b206b
Bradley Grainger <bradley.grainger@logos.com>
2017-02-18 11:38:33 -0800

    Code style: remove braces around single-line statements.

--- src/MySqlConnector/MySqlClient/MySqlBulkLoader.cs
+++ src/MySqlConnector/MySqlClient/MySqlBulkLoader.cs
@@ -66,95 +66,75 @@ namespace MySql.Data.MySqlClient
         {
             StringBuilder sqlCommandMain = new StringBuilder("LOAD DATA ");
             if (Priority == MySqlBulkLoaderPriority.Low)
-            {
                 sqlCommandMain.Append("LOW_PRIORITY ");
-            }
             else if (Priority == MySqlBulkLoaderPriority.Concurrent)
-            {
                 sqlCommandMain.Append("CONCURRENT ");
-            }
-            if (Local)
-            {
-                sqlCommandMain.Append("LOCAL ");
-            }
-            sqlCommandMain.Append("INFILE ");
-            if (System.IO.Path.DirectorySeparatorChar != '\\')
-            {
-                sqlCommandMain.AppendFormat("'{0}' ", FileName);
-            }
-            else
-            {
-                sqlCommandMain.AppendFormat("'{0}' ", FileName.Replace("\\", "\\\\"));
-            }
-            if (ConflictOption == MySqlBulkLoaderConflictOption.Ignore)
-            {
-                sqlCommandMain.Append("IGNORE ");
-            }
-            else if (ConflictOption == MySqlBulkLoaderConflictOption.Replace)
-            {
-                sqlCommandMain.Append("REPLACE ");
-            }
-            sqlCommandMain.AppendFormat("INTO TABLE {0} ", TableName);
-            if (CharacterSet != null)
-            {
-                sqlCommandMain.AppendFormat("CHARACTER SET {0} ", CharacterSet);
-            }
 
-            StringBuilder sqlCommandFragment = new StringBuilder(string.Empty);
+            if (Local)
+                sqlCommandMain.Append("LOCAL ");
+
+            sqlCommandMain.Append("INFILE ");
+
+            if (System.IO.Path.DirectorySeparatorChar != '\\')
+                sqlCommandMain.AppendFormat("'{0}' ", FileName);
+            else
+                sqlCommandMain.AppendFormat("'{0}' ", FileName.Replace("\\", "\\\\"));
+
+            if (ConflictOption == MySqlBulkLoaderConflictOption.Ignore)
+                sqlCommandMain.Append("IGNORE ");
+            else if (ConflictOption == MySqlBulkLoaderConflictOption.Replace)
+                sqlCommandMain.Append("REPLACE ");
+
+            sqlCommandMain.AppendFormat("INTO TABLE {0} ", TableName);
+
+            if (CharacterSet != null)
+                sqlCommandMain.AppendFormat("CHARACTER SET {0} ", CharacterSet);
+
+            StringBuilder sqlCommandFragment = new StringBuilder();
             if (FieldTerminator != defaultFieldTerminator)
-            {
                 sqlCommandFragment.AppendFormat("TERMINATED BY \'{0}\' ", FieldTerminator);
-            }
+
             if (FieldQuotationCharacter != 0)
-            {
                 sqlCommandFragment.AppendFormat("{0} ENCLOSED BY \'{1}\' ", (FieldQuotationOptional ? "OPTIONALLY" : ""), FieldQuotationCharacter);
-            }
+
             if (EscapeCharacter != defaultEscapeCharacter && EscapeCharacter != 0)
-            {
                 sqlCommandFragment.AppendFormat("ESCAPED BY \'{0}\' ", EscapeCharacter);
-            }
+
             if (sqlCommandFragment.Length > 0)
             {
                 sqlCommandMain.AppendFormat("FIELDS {0}", sqlCommandFragment.ToString());
+                sqlCommandFragment.Clear();
             }
 
-            sqlCommandFragment.Clear();
             if (LinePrefix != null && LinePrefix.Length > 0)
-            {
                 sqlCommandFragment.AppendFormat("STARTING BY \'{0}\' ", LinePrefix);
-            }
+
             if (LineTerminator != defaultLineTerminator)
-            {
                 sqlCommandFragment.AppendFormat("TERMINATED BY \'{0}\' ", LineTerminator);
-            }
+
             if (sqlCommandFragment.Length > 0)
-            {
                 sqlCommandMain.AppendFormat("LINES {0}", sqlCommandFragment.ToString());
-            }
 
             if (NumberOfLinesToSkip > 0)
-            {
                 sqlCommandMain.AppendFormat("IGNORE {0} LINES ", NumberOfLinesToSkip);
-            }
+
             if (Columns.Count > 0)
             {
                 sqlCommandMain.Append("(");
                 sqlCommandMain.Append(Columns[0]);
                 for (int i = 1; i < Columns.Count; i++)
-                {
                     sqlCommandMain.AppendFormat(",{0}", Columns[i]);
-                }
                 sqlCommandMain.Append(") ");
             }
+
             if (Expressions.Count > 0)
             {
                 sqlCommandMain.Append("SET ");
                 sqlCommandMain.Append(Expressions[0]);
-                for (int j = 1; j < Expressions.Count; j++)
-                {
-                    sqlCommandMain.AppendFormat(",{0}", Expressions[j]);
-                }
+                for (int i = 1; i < Expressions.Count; i++)
+                    sqlCommandMain.AppendFormat(",{0}", Expressions[i]);
             }
+
             return sqlCommandMain.ToString();
         }
 
