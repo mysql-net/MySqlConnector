596a1397a367b3957297d6e385681b7c8537a1e8
Bradley Grainger <bgrainger@gmail.com>
2017-04-07 20:16:15 -0700

    Verify that invalid SQL throws the right exception.

---
+++ src/MySqlConnector/MySqlClient/MySqlErrorCode.cs
@@ -0,0 +1,13 @@
+namespace MySql.Data.MySqlClient
+{
+	/// <summary>
+	/// MySQL Server error codes. Taken from <a href="https://dev.mysql.com/doc/refman/5.7/en/error-messages-server.html">Server Error Codes and Messages</a>.
+	/// </summary>
+	public enum MySqlErrorCode
+	{
+		/// <summary>
+		/// You have an error in your SQL syntax (ER_PARSE_ERROR).
+		/// </summary>
+		ParseError = 1064,
+	}
+}
