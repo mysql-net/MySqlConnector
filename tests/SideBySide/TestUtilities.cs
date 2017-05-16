646ddb2134f118877e486c523cf6fd76b55f0fe2
Bradley Grainger <bgrainger@gmail.com>
2017-05-15 20:06:03 -0700

    Remove method that has moved to AppConfig.

--- tests/SideBySide/TestUtilities.cs
+++ tests/SideBySide/TestUtilities.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Globalization;
+﻿using System.Diagnostics;
 using Xunit;
 
 namespace SideBySide
@@ -31,30 +29,5 @@ namespace SideBySide
 			var elapsed = stopwatch.ElapsedMilliseconds;
 			Assert.InRange(elapsed, minimumMilliseconds, minimumMilliseconds + lengthMilliseconds * AppConfig.TimeoutDelayFactor);
 		}
-
-		public static Version ParseServerVersion(string serverVersion)
-		{
-			// copied from MySql.Data.MySqlClient.ServerVersion
-
-			var last = 0;
-			var index = serverVersion.IndexOf('.', last);
-			var major = int.Parse(serverVersion.Substring(last, index - last), CultureInfo.InvariantCulture);
-			last = index + 1;
-
-			index = serverVersion.IndexOf('.', last);
-			var minor = int.Parse(serverVersion.Substring(last, index - last), CultureInfo.InvariantCulture);
-			last = index + 1;
-
-			do
-			{
-				index++;
-			} while (index < serverVersion.Length && serverVersion[index] >= '0' && serverVersion[index] <= '9');
-			var build = int.Parse(serverVersion.Substring(last, index - last), CultureInfo.InvariantCulture);
-
-			return new Version(major, minor, build);
-		}
-
-		public static bool SupportsJson(string serverVersion) =>
-			ParseServerVersion(serverVersion).CompareTo(new Version(5, 7)) >= 0;
 	}
 }
