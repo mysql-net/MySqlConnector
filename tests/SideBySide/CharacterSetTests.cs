83c1cea6f22ac4547e157a2eb93fccc85e6850a5
Bradley Grainger <bgrainger@gmail.com>
2017-09-22 23:24:26 -0700

    Handle all values in GetBytesPerCharacter. Fixes #337

---
+++ tests/SideBySide/CharacterSetTests.cs
@@ -0,0 +1,33 @@
+using Dapper;
+using MySql.Data.Serialization;
+using Xunit;
+
+namespace SideBySide
+{
+	public class CharacterSetTests : IClassFixture<DatabaseFixture>
+	{
+		public CharacterSetTests(DatabaseFixture database)
+		{
+			m_database = database;
+		}
+
+#if !BASELINE
+		[Fact]
+		public void MaxLength()
+		{
+			using (var reader = m_database.Connection.ExecuteReader(@"select coll.ID, cs.MAXLEN from information_schema.collations coll inner join information_schema.character_sets cs using(CHARACTER_SET_NAME);"))
+			{
+				while (reader.Read())
+				{
+					var characterSet = (CharacterSet) reader.GetInt32(0);
+					var maxLength = reader.GetInt32(1);
+
+					Assert.Equal(maxLength, SerializationUtility.GetBytesPerCharacter(characterSet));
+				}
+			}
+		}
+#endif
+
+		readonly DatabaseFixture m_database;
+	}
+}
