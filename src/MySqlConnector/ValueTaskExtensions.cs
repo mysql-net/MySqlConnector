ad6b637aca2fe6ad9758f943ea450f4970a777c8
Bradley Grainger <bgrainger@gmail.com>
2017-10-20 14:46:59 -0700

    Use await instead of hand-written continuation.
    The compiler-generated state machine reduces allocations and performs better in benchmarks.

--- src/MySqlConnector/ValueTaskExtensions.cs
+++ src/MySqlConnector/ValueTaskExtensions.cs
@@ -1,15 +1,11 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 
 namespace MySql.Data
 {
 	internal static class ValueTaskExtensions
 	{
-		public static ValueTask<TResult> ContinueWith<T, TResult>(this ValueTask<T> valueTask, Func<T, ValueTask<TResult>> continuation)
-		{
-			return valueTask.IsCompleted ? continuation(valueTask.Result) :
-				new ValueTask<TResult>(valueTask.AsTask().ContinueWith(task => continuation(task.GetAwaiter().GetResult()).AsTask()).Unwrap());
-		}
+		public static async ValueTask<TResult> ContinueWith<T, TResult>(this ValueTask<T> valueTask, Func<T, ValueTask<TResult>> continuation) => await continuation(await valueTask);
 
 		public static ValueTask<T> FromException<T>(Exception exception) => new ValueTask<T>(Utility.TaskFromException<T>(exception));
 	}
