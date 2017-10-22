312e5622f5e62446bd12affa7ce3e17ce75726e8
Bradley Grainger <bgrainger@gmail.com>
2017-10-21 17:50:18 -0700

    Expand "DataTypes" schema.
    Define all the columns from the MSDN documentation; some are still filled with default values.
    
    Backwards-compatibility is checked by: https://github.com/bgrainger/MySqlSchemaChecker
    
    Results are: https://github.com/mysql-net/MySqlConnector/wiki/DataTypes-Schema-Collection

--- src/MySqlConnector/MySqlClient/SchemaProvider.cs
+++ src/MySqlConnector/MySqlClient/SchemaProvider.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
+using MySql.Data.MySqlClient.Types;
 
 namespace MySql.Data.MySqlClient
 {
@@ -45,32 +45,85 @@ namespace MySql.Data.MySqlClient
 
 		private void FillDataTypes(DataTable dataTable)
 		{
-			dataTable.Columns.AddRange(new[] { // The names come from DbMetaDataColumnNames
-				new DataColumn("DataType", typeof(string)),
+			dataTable.Columns.AddRange(new[]
+			{
 				new DataColumn("TypeName", typeof(string)),
 				new DataColumn("ProviderDbType", typeof(int)),
-				new DataColumn("IsUnsigned", typeof(bool))
+				new DataColumn("ColumnSize", typeof(long)),
+				new DataColumn("CreateFormat", typeof(string)),
+				new DataColumn("CreateParameters", typeof(string)),
+				new DataColumn("DataType", typeof(string)),
+				new DataColumn("IsAutoIncrementable", typeof(bool)),
+				new DataColumn("IsBestMatch", typeof(bool)),
+				new DataColumn("IsCaseSensitive", typeof(bool)),
+				new DataColumn("IsFixedLength", typeof(bool)),
+				new DataColumn("IsFixedPrecisionScale", typeof(bool)),
+				new DataColumn("IsLong", typeof(bool)),
+				new DataColumn("IsNullable", typeof(bool)),
+				new DataColumn("IsSearchable", typeof(bool)),
+				new DataColumn("IsSearchableWithLike", typeof(bool)),
+				new DataColumn("IsUnsigned", typeof(bool)),
+				new DataColumn("MaximumScale", typeof(short)),
+				new DataColumn("MinimumScale", typeof(short)),
+				new DataColumn("IsConcurrencyType", typeof(bool)),
+				new DataColumn("IsLiteralSupported", typeof(bool)),
+				new DataColumn("LiteralPrefix", typeof(string)),
+				new DataColumn("LiteralSuffix", typeof(string)),
+				new DataColumn("NativeDataType", typeof(string)),
 			});
 
-			// Column type mappings:
-			var colTypes = Types.TypeMapper.Instance.GetColumnMappings();
-			foreach (var map in colTypes)
+			var clrTypes = new HashSet<string>();
+			foreach (var columnType in TypeMapper.Instance.GetColumnTypeMetadata())
 			{
-				var dbTypeMap = map.DbTypeMapping;
-				var dbType = dbTypeMap.DbTypes.FirstOrDefault();
-				dataTable.Rows.Add(dbTypeMap.ClrType.FullName, map.DataTypeName, (int)dbType, map.Unsigned);
-			}
+				// hard-code a few types to not appear in the schema table
+				var mySqlDbType = columnType.MySqlDbType;
+				if (mySqlDbType == MySqlDbType.Decimal || mySqlDbType == MySqlDbType.Newdate || mySqlDbType == MySqlDbType.Null || mySqlDbType == MySqlDbType.VarString)
+					continue;
+				if (mySqlDbType == MySqlDbType.Bool && columnType.IsUnsigned)
+					continue;
 
-			// Data type mappings:
-			foreach (MySqlDbType mapItem in Enum.GetValues(typeof(MySqlDbType)))
-			{
-				var typeName = Enum.GetName(typeof(MySqlDbType), mapItem);
-				var dbType = Types.TypeMapper.Instance.GetDbTypeForMySqlDbType(mapItem);
-				var map = Types.TypeMapper.Instance.GetDbTypeMapping(dbType);
-				if (map != null) // MySqlDbType.Set is not supported by the mapper.
-				{
-					dataTable.Rows.Add(map.ClrType.FullName, Enum.GetName(typeof(MySqlDbType), mapItem).ToLower(), (int)dbType, typeName.Contains("UInt") || typeName.Contains("UByte"));
-				}
+				// set miscellaneous properties in code (rather than being data-driven)
+				var clrType = columnType.DbTypeMapping.ClrType;
+				var clrTypeName = clrType.ToString();
+				var dataTypeName = mySqlDbType == MySqlDbType.Guid ? "GUID" :
+					mySqlDbType == MySqlDbType.Bool ? "BOOL" : columnType.DataTypeName;
+				var isAutoIncrementable = mySqlDbType == MySqlDbType.Byte || mySqlDbType == MySqlDbType.Int16 || mySqlDbType == MySqlDbType.Int24 || mySqlDbType == MySqlDbType.Int32 || mySqlDbType == MySqlDbType.Int64 ||
+					mySqlDbType == MySqlDbType.UByte || mySqlDbType == MySqlDbType.UInt16 || mySqlDbType == MySqlDbType.UInt24 || mySqlDbType == MySqlDbType.UInt32 || mySqlDbType == MySqlDbType.UInt64;
+				var isBestMatch = clrTypes.Add(clrTypeName);
+				var isFixedLength = isAutoIncrementable ||
+					mySqlDbType == MySqlDbType.Date || mySqlDbType == MySqlDbType.DateTime || mySqlDbType == MySqlDbType.Time || mySqlDbType == MySqlDbType.Timestamp ||
+					mySqlDbType == MySqlDbType.Double || mySqlDbType == MySqlDbType.Float || mySqlDbType == MySqlDbType.Year || mySqlDbType == MySqlDbType.Guid || mySqlDbType == MySqlDbType.Bool;
+				var isFixedPrecisionScale = isFixedLength ||
+					mySqlDbType == MySqlDbType.Bit || mySqlDbType == MySqlDbType.NewDecimal;
+				var isLong = mySqlDbType == MySqlDbType.Blob || mySqlDbType == MySqlDbType.MediumBlob || mySqlDbType == MySqlDbType.LongBlob;
+
+				// map ColumnTypeMetadata to the row for this data type
+				var createFormatParts = columnType.CreateFormat.Split(';');
+				dataTable.Rows.Add(
+					dataTypeName,
+					(int) mySqlDbType,
+					columnType.ColumnSize,
+					createFormatParts[0],
+					createFormatParts.Length == 1 ? null : createFormatParts[1],
+					clrTypeName,
+					isAutoIncrementable,
+					isBestMatch,
+					false,
+					isFixedLength,
+					isFixedPrecisionScale,
+					isLong,
+					true,
+					clrType != typeof(byte[]),
+					clrType == typeof(string),
+					columnType.IsUnsigned,
+					DBNull.Value,
+					DBNull.Value,
+					DBNull.Value,
+					true,
+					DBNull.Value,
+					DBNull.Value,
+					null
+				);
 			}
 		}
 
--- src/MySqlConnector/MySqlClient/Types/ColumnTypeMetadata.cs
+++ src/MySqlConnector/MySqlClient/Types/ColumnTypeMetadata.cs
@@ -4,25 +4,29 @@ namespace MySql.Data.MySqlClient.Types
 	{
 		public static string CreateLookupKey(string columnTypeName, bool isUnsigned, int length) => $"{columnTypeName}|{(isUnsigned ? "u" : "s")}|{length}";
 
-		public ColumnTypeMetadata(string dataTypeName, DbTypeMapping dbTypeMapping, MySqlDbType mySqlDbType, bool unsigned = false, bool binary = false, int length = 0, string simpleDataTypeName = null)
+		public ColumnTypeMetadata(string dataTypeName, DbTypeMapping dbTypeMapping, MySqlDbType mySqlDbType, bool isUnsigned = false, bool binary = false, int length = 0, string simpleDataTypeName = null, string createFormat = null, long columnSize = 0)
 		{
 			DataTypeName = dataTypeName;
 			SimpleDataTypeName = simpleDataTypeName ?? dataTypeName;
+			CreateFormat = createFormat ?? (dataTypeName + (isUnsigned ? " UNSIGNED" : ""));
 			DbTypeMapping = dbTypeMapping;
 			MySqlDbType = mySqlDbType;
-			Unsigned = unsigned;
+			ColumnSize = columnSize;
+			IsUnsigned = isUnsigned;
 			Binary = binary;
 			Length = length;
 		}
 
 		public string DataTypeName { get; }
 		public string SimpleDataTypeName { get; }
+		public string CreateFormat { get; }
 		public DbTypeMapping DbTypeMapping { get; }
 		public MySqlDbType MySqlDbType { get; }
 		public bool Binary { get; }
-		public bool Unsigned { get; }
+		public long ColumnSize { get; }
+		public bool IsUnsigned { get; }
 		public int Length { get; }
 
-		public string CreateLookupKey() => CreateLookupKey(DataTypeName, Unsigned, Length);
+		public string CreateLookupKey() => CreateLookupKey(DataTypeName, IsUnsigned, Length);
 	}
 }
