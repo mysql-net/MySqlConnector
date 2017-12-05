85047a908ed688ef30c0d2648f2368428522e24b
Bradley Grainger <bgrainger@gmail.com>
2017-12-04 16:41:05 -0800

    Add missing ConfigureAwait(false).
    The 'await' was added in ad6b637aca2fe6ad9758f943ea450f4970a777c8.

--- src/MySqlConnector/Utilities/ValueTaskExtensions.cs
+++ src/MySqlConnector/Utilities/ValueTaskExtensions.cs
@@ -5,7 +5,7 @@ namespace MySqlConnector.Utilities
 {
 	internal static class ValueTaskExtensions
 	{
-		public static async ValueTask<TResult> ContinueWith<T, TResult>(this ValueTask<T> valueTask, Func<T, ValueTask<TResult>> continuation) => await continuation(await valueTask);
+		public static async ValueTask<TResult> ContinueWith<T, TResult>(this ValueTask<T> valueTask, Func<T, ValueTask<TResult>> continuation) => await continuation(await valueTask.ConfigureAwait(false)).ConfigureAwait(false);
 
 		public static ValueTask<T> FromException<T>(Exception exception) => new ValueTask<T>(Utility.TaskFromException<T>(exception));
 	}
