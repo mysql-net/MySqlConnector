bd73c69db989a6a06dc6c47069b9f2e77aa89543
Caleb Lloyd <caleblloyd@gmail.com>
2018-06-12 23:07:20 -0400

    update performance tests to netcoreapp2.1
    Signed-off-by: Caleb Lloyd <caleblloyd@gmail.com>

--- tests/MySqlConnector.Performance/AppConfig.cs
+++ tests/MySqlConnector.Performance/AppConfig.cs
@@ -1,14 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Reflection;
 using Microsoft.Extensions.Configuration;
 
 namespace MySqlConnector.Performance
 {
 	public static class AppConfig
 	{
-		public static IConfigurationRoot Config = new ConfigurationBuilder()
-			.SetBasePath(Directory.GetCurrentDirectory())
+		public static IConfigurationRoot Config => LazyConfig.Value;
+
+		private static readonly Lazy<IConfigurationRoot> LazyConfig = new Lazy<IConfigurationRoot>(() => new ConfigurationBuilder()
+			.SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
 			.AddJsonFile("appsettings.json")
 			.AddJsonFile("config.json")
-			.Build();
+			.Build());
 	}
 }
