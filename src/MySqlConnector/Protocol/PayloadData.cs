20717e226643fd4d089e1acd3b376bcd95fa3b47
Bradley Grainger <bgrainger@gmail.com>
2018-08-28 10:37:50 -0700

    Use readonly struct.

--- src/MySqlConnector/Protocol/PayloadData.cs
+++ src/MySqlConnector/Protocol/PayloadData.cs
@@ -3,7 +3,7 @@ using System.Buffers;
 
 namespace MySqlConnector.Protocol
 {
-	internal struct PayloadData : IDisposable
+	internal readonly struct PayloadData : IDisposable
 	{
 		public PayloadData(byte[] data, bool isPooled = false)
 		{
