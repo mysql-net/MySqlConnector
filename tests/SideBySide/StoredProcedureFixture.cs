027dcc255a3a98216f3c9a091fba8f5740f0f118
Bradley Grainger <bgrainger@gmail.com>
2019-09-04 12:55:52 -0700

    Rewrite fixture setup code as one long SQL statement.

--- tests/SideBySide/StoredProcedureFixture.cs
+++ tests/SideBySide/StoredProcedureFixture.cs
@@ -13,23 +13,26 @@ namespace SideBySide
 				) RETURNS VARCHAR(63)
 				BEGIN
 					RETURN name;
-				END");
-			Connection.Execute(@"DROP FUNCTION IF EXISTS failing_function;
+				END;
+
+				DROP FUNCTION IF EXISTS failing_function;
 				CREATE FUNCTION failing_function()
 				RETURNS INT
 				BEGIN
 					DECLARE v1 INT;
 					SELECT c1 FROM table_that_does_not_exist INTO v1;
 					RETURN v1;
-				END");
-			Connection.Execute(@"DROP PROCEDURE IF EXISTS echop;
+				END;
+
+				DROP PROCEDURE IF EXISTS echop;
 				CREATE PROCEDURE echop(
 					IN name VARCHAR(63)
 				)
 				BEGIN
 					SELECT name;
-				END");
-			Connection.Execute(@"DROP PROCEDURE IF EXISTS circle;
+				END;
+
+				DROP PROCEDURE IF EXISTS circle;
 				CREATE PROCEDURE circle(
 					IN radius DOUBLE,
 					IN height DOUBLE,
@@ -47,15 +50,17 @@ namespace SideBySide
 					SELECT area * height INTO volume;
 					SELECT 'circle' INTO shape;
 					SELECT CONCAT(name, shape);
-				END");
-			Connection.Execute(@"DROP PROCEDURE IF EXISTS out_string;
+				END;
+
+				DROP PROCEDURE IF EXISTS out_string;
 				CREATE PROCEDURE out_string(
 					OUT value VARCHAR(100)
 				)
 				BEGIN
 					SELECT 'test value' INTO value;
-				END");
-			Connection.Execute(@"DROP PROCEDURE IF EXISTS out_null;
+				END;
+
+				DROP PROCEDURE IF EXISTS out_null;
 				CREATE PROCEDURE out_null(
 					OUT string_value VARCHAR(100),
 					OUT int_value INT
@@ -63,8 +68,9 @@ namespace SideBySide
 				BEGIN
 					SELECT NULL INTO string_value;
 					SELECT NULL INTO int_value;
-				END");
-			Connection.Execute(@"drop table if exists sproc_multiple_rows;
+				END;
+
+				drop table if exists sproc_multiple_rows;
 				create table sproc_multiple_rows (
 					value integer not null primary key auto_increment,
 					name text not null
@@ -77,21 +83,24 @@ namespace SideBySide
 				(5, 'five'),
 				(6, 'six'),
 				(7, 'seven'),
-				(8, 'eight');");
-			Connection.Execute(@"drop procedure if exists number_multiples;
+				(8, 'eight');
+
+				drop procedure if exists number_multiples;
 				create procedure number_multiples (in factor int)
 				begin
 					select name from sproc_multiple_rows
 					where mod(value, factor) = 0
 					order by name;
-				end;");
-			Connection.Execute(@"drop procedure if exists multiple_result_sets;
+				end;
+
+				drop procedure if exists multiple_result_sets;
 				create procedure multiple_result_sets (in pivot int)
 				begin
 					select name from sproc_multiple_rows where value < pivot order by name;
 					select name from sproc_multiple_rows where value > pivot order by name;
-				end;");
-			Connection.Execute(@"drop procedure if exists number_lister;
+				end;
+
+				drop procedure if exists number_lister;
 				create procedure number_lister (inout high int)
 				begin
 					DECLARE i int;
@@ -103,17 +112,19 @@ namespace SideBySide
 						SET i = i + 1;
 					END WHILE;
 					SET high = high + 1;
-				end;");
-			Connection.Execute(@"drop procedure if exists `dotted.name`;
+				end;
+
+				drop procedure if exists `dotted.name`;
 				create procedure `dotted.name`()
 				begin
 					select 1, 2, 3;
-				end;");
-			Connection.Execute(@"DROP PROCEDURE IF EXISTS `GetTime`;
+				end;
+
+				DROP PROCEDURE IF EXISTS `GetTime`;
 				CREATE PROCEDURE `GetTime`(OUT OutTime TIME)
 				BEGIN
 					SET OutTime = CURTIME();
-				END");
+				END;");
 
 			if (AppConfig.SupportsJson)
 			{
