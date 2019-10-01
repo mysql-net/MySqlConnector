9b8f3a4d8259144b52990d804f1dca303314289f
Bradley Grainger <bgrainger@gmail.com>
2019-10-01 11:21:56 -0700

    Use stackalloc expressions.

--- src/MySqlConnector/Core/Row.cs
+++ src/MySqlConnector/Core/Row.cs
@@ -425,15 +425,9 @@ namespace MySqlConnector.Core
 			unsafe
 			{
 				if (guidFormat == MySqlGuidFormat.Binary16)
-				{
-					ReadOnlySpan<byte> guid = stackalloc byte[16] { bytes[3], bytes[2], bytes[1], bytes[0], bytes[5], bytes[4], bytes[7], bytes[6], bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15] };
-					return new Guid(guid);
-				}
+					return new Guid(stackalloc byte[16] { bytes[3], bytes[2], bytes[1], bytes[0], bytes[5], bytes[4], bytes[7], bytes[6], bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15] });
 				if (guidFormat == MySqlGuidFormat.TimeSwapBinary16)
-				{
-					ReadOnlySpan<byte> guid = stackalloc byte[16] { bytes[7], bytes[6], bytes[5], bytes[4], bytes[3], bytes[2], bytes[1], bytes[0], bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15] };
-					return new Guid(guid);
-				}
+					return new Guid(stackalloc byte[16] { bytes[7], bytes[6], bytes[5], bytes[4], bytes[3], bytes[2], bytes[1], bytes[0], bytes[8], bytes[9], bytes[10], bytes[11], bytes[12], bytes[13], bytes[14], bytes[15] });
 				return new Guid(bytes);
 			}
 #endif
