0e88f5054d80002b702c9b9930a4f1be51bbe6f4
Bradley Grainger <bgrainger@gmail.com>
2019-10-11 21:44:07 -0700

    Remove unnecessary IValueTaskSource implementation.
    .NET Standard 2.1 introduces overloads on Socket that implement an efficient API.

--- src/MySqlConnector/Protocol/Serialization/SocketByteHandler.cs
+++ src/MySqlConnector/Protocol/Serialization/SocketByteHandler.cs
@@ -1,42 +1,28 @@
-#if NETSTANDARD2_1 || NETCOREAPP3_0
-#define VALUETASKSOURCE
-#endif
-
 using System;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-#if VALUETASKSOURCE
-using System.Threading.Tasks.Sources;
-#endif
 using MySql.Data.MySqlClient;
 using MySqlConnector.Utilities;
 
 namespace MySqlConnector.Protocol.Serialization
 {
 	internal sealed class SocketByteHandler : IByteHandler
-#if VALUETASKSOURCE
-		, IValueTaskSource<int>
-#endif
 	{
 		public SocketByteHandler(Socket socket)
 		{
 			m_socket = socket;
-#if VALUETASKSOURCE
-			m_valueTaskSource = new ManualResetValueTaskSourceCore<int> { RunContinuationsAsynchronously = true };
-			m_socketEventArgs = new SocketAsyncEventArgs();
-			m_socketEventArgs.Completed += (s, e) => PropagateSocketAsyncEventArgsStatus();
-#else
+#if !NETSTANDARD2_1 && !NETCOREAPP3_0
 			m_socketAwaitable = new SocketAwaitable(new SocketAsyncEventArgs());
 #endif
 			m_closeSocket = socket.Dispose;
 			RemainingTimeout = Constants.InfiniteTimeout;
 		}
 
-#if VALUETASKSOURCE
-		public void Dispose() => m_socketEventArgs.Dispose();
-#else
+#if !NETSTANDARD2_1 && !NETCOREAPP3_0
 		public void Dispose() => m_socketAwaitable.EventArgs.Dispose();
+#else
+		public void Dispose() { }
 #endif
 
 		public int RemainingTimeout { get; set; }
@@ -88,30 +74,17 @@ namespace MySqlConnector.Protocol.Serialization
 			var timerId = RemainingTimeout == Constants.InfiniteTimeout ? 0 :
 				RemainingTimeout <= 0 ? throw MySqlException.CreateForTimeout() :
 				TimerQueue.Instance.Add(RemainingTimeout, m_closeSocket);
-#if VALUETASKSOURCE
-			m_socketEventArgs.SetBuffer(buffer);
-#else
+#if !NETSTANDARD2_1 && !NETCOREAPP3_0
 			m_socketAwaitable.EventArgs.SetBuffer(buffer);
 #endif
 			int bytesRead;
 			try
 			{
-#if VALUETASKSOURCE
-				m_valueTaskSource.Reset();
-				if (!m_socket.ReceiveAsync(m_socketEventArgs))
-				{
-					if (m_socketEventArgs.SocketError != SocketError.Success)
-						throw new SocketException((int) m_socketEventArgs.SocketError);
-					else
-						bytesRead = m_socketEventArgs.BytesTransferred;
-				}
-				else
-				{
-					bytesRead = await new ValueTask<int>(this, m_valueTaskSource.Version).ConfigureAwait(false);
-				}
-#else
+#if !NETSTANDARD2_1 && !NETCOREAPP3_0
 				await m_socket.ReceiveAsync(m_socketAwaitable);
 				bytesRead = m_socketAwaitable.EventArgs.BytesTransferred;
+#else
+				bytesRead = await m_socket.ReceiveAsync(buffer, SocketFlags.None).ConfigureAwait(false);
 #endif
 			}
 			catch (SocketException ex)
@@ -149,44 +122,19 @@ namespace MySqlConnector.Protocol.Serialization
 			}
 		}
 
-#if VALUETASKSOURCE
-		private ValueTask<int> DoWriteBytesAsync(ReadOnlyMemory<byte> data)
-		{
-			m_socketEventArgs.SetBuffer(MemoryMarshal.AsMemory(data));
-			m_valueTaskSource.Reset();
-			if (!m_socket.SendAsync(m_socketEventArgs))
-				PropagateSocketAsyncEventArgsStatus();
-			return new ValueTask<int>(this, m_valueTaskSource.Version);
-		}
-#else
 		private async ValueTask<int> DoWriteBytesAsync(ReadOnlyMemory<byte> data)
 		{
+#if !NETSTANDARD2_1 && !NETCOREAPP3_0
 			m_socketAwaitable.EventArgs.SetBuffer(MemoryMarshal.AsMemory(data));
 			await m_socket.SendAsync(m_socketAwaitable);
+#else
+			await m_socket.SendAsync(data, SocketFlags.None).ConfigureAwait(false);
+#endif
 			return 0;
 		}
-#endif
 
-#if VALUETASKSOURCE
-		int IValueTaskSource<int>.GetResult(short token) => m_valueTaskSource.GetResult(token);
-		ValueTaskSourceStatus IValueTaskSource<int>.GetStatus(short token) => m_valueTaskSource.GetStatus(token);
-		void IValueTaskSource<int>.OnCompleted(Action<object?> continuation, object? state, short token, ValueTaskSourceOnCompletedFlags flags) =>
-			m_valueTaskSource.OnCompleted(continuation, state, token, flags);
-
-		private void PropagateSocketAsyncEventArgsStatus()
-		{
-			if (m_socketEventArgs.SocketError != SocketError.Success)
-				m_valueTaskSource.SetException(new SocketException((int) m_socketEventArgs.SocketError));
-			else
-				m_valueTaskSource.SetResult(m_socketEventArgs.BytesTransferred);
-		}
-#endif
-
-			readonly Socket m_socket;
-#if VALUETASKSOURCE
-		ManualResetValueTaskSourceCore<int> m_valueTaskSource; // mutable struct; do not make this readonly
-		readonly SocketAsyncEventArgs m_socketEventArgs;
-#else
+		readonly Socket m_socket;
+#if !NETSTANDARD2_1 && !NETCOREAPP3_0
 		readonly SocketAwaitable m_socketAwaitable;
 #endif
 		readonly Action m_closeSocket;
