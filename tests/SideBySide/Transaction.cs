74445f01fab75de0aa539be50e895f0c75a3ae6d
John Battye <John.Battye@ipfin.co.uk>
2020-03-27 16:03:24 +0000

    added thread id to sql query s tests run in parallel
    Signed-off-by: John Battye <John.Battye@ipfin.co.uk>

--- tests/SideBySide/Transaction.cs
+++ tests/SideBySide/Transaction.cs
@@ -70,7 +70,7 @@ namespace SideBySide
 			}
 
 			m_connection.Execute(@"set global general_log = 0;");
-			var results = connection.Query<string>(@"select convert(argument USING utf8) from mysql.general_log order by event_time desc limit 10;");
+			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
 			var lastIsolationLevelQuery = results.First(x => x.ToLower().Contains("set transaction isolation level"));
 			Assert.Contains(expectedTransactionIsolationLevel.ToLower(), lastIsolationLevelQuery.ToLower());
 		}
@@ -93,7 +93,7 @@ namespace SideBySide
 			}
 
 			m_connection.Execute(@"set global general_log = 0;");
-			var results = connection.Query<string>(@"select convert(argument USING utf8) from mysql.general_log order by event_time desc limit 10;");
+			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
 			var lastStartTransactionQuery = results.First(x => x.ToLower().Contains("start transaction"));
 			Assert.Equal(expectedTransactionIsolationLevel.ToLower(), lastStartTransactionQuery.ToLower());
 		}
