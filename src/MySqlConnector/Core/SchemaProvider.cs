39e8513ebd951acdb88e5d889b6e70fb15be0d3a
Vincent DARON <vda@dfakto.com>
2020-04-06 12:15:16 +0200

    Change DataSourceProductName to MySQL Change DataSourceProductVersionNormalized to have a String Comparable result
    Signed-off-by: Vincent DARON <vda@dfakto.com>

--- src/MySqlConnector/Core/SchemaProvider.cs
+++ src/MySqlConnector/Core/SchemaProvider.cs
@@ -84,9 +84,9 @@ namespace MySqlConnector.Core
 
 			var row = dataTable.NewRow();
 			row["CompositeIdentifierSeparatorPattern"] = @"\.";
-			row["DataSourceProductName"] = "MysqlConnector";
+			row["DataSourceProductName"] = "MySQL";
 			row["DataSourceProductVersion"] = m_connection.ServerVersion;
-			row["DataSourceProductVersionNormalized"] = m_connection.Session.ServerVersion.Version.ToString();
+			row["DataSourceProductVersionNormalized"] = GetVersion(m_connection.Session.ServerVersion.Version);
 			row["GroupByBehavior"] = GroupByBehavior.Unrelated;
 			row["IdentifierPattern"] = @"(^\[\p{Lo}\p{Lu}\p{Ll}_@#][\p{Lo}\p{Lu}\p{Ll}\p{Nd}@$#_]*$)|(^\[[^\]\0]|\]\]+\]$)|(^\""[^\""\0]|\""\""+\""$)";
 			row["IdentifierCase"] = IdentifierCase.Insensitive;
@@ -107,6 +107,7 @@ namespace MySqlConnector.Core
 			dataTable.Rows.Add(row);
 		}
 
+		private string GetVersion(Version v) => $"{v.Major:00}.{v.Minor:00}.{v.Build:0000}";
 
 		private void FillMetadataCollections(DataTable dataTable)
 		{
