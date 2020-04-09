95d744170de4b206ecf95095c8b38bfb89a00a54
Bradley Grainger <bgrainger@gmail.com>
2020-04-09 12:32:54 -0700

    Fix transaction isolation level tests for MySQL 5.6.
    MySQL 5.6's general_log table only has second resolution, so ordering by
    event_time doesn't put most recent events first. Instead, assume the
    general_log table doesn't have many entries, select them all, and use
    the last (i.e., most recent) one.
    
    MariaDB puts both commands in the same entry in the general_log table,
    so we can't assume 'start transaction' will be at the beginning.

--- tests/SideBySide/Transaction.cs
+++ tests/SideBySide/Transaction.cs
@@ -64,22 +64,16 @@ namespace SideBySide
 		public void DbConnectionIsolationLevel(IsolationLevel inputIsolationLevel, string expectedTransactionIsolationLevel)
 		{
 			DbConnection connection = m_connection;
-			m_connection.Execute(@"set global log_output = 'table';");
-			m_connection.Execute(@"set global general_log = 1;");
+			connection.Execute(@"set global log_output = 'table';");
+			connection.Execute(@"set global general_log = 1;");
 			using (var trans = connection.BeginTransaction(inputIsolationLevel))
-			{
 				trans.Commit();
-			}
 
-			m_connection.Execute(@"set global general_log = 0;");
-			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
-			var lastIsolationLevelQuery = results.First(x => x.ToLower().Contains("isolation"));
+			connection.Execute(@"set global general_log = 0;");
 
-			if (IsMySqlAndVersionLessThan57(m_connection.ServerVersion))
-			{
-				Assert.Contains("serializable", lastIsolationLevelQuery.ToLower());
-				return;
-			}
+			// we could use @tx_isolation (in MySQL 5.6/5.7) and @transaction_isolation (in MySQL 5.7/8.0), but would require a different query for different server versions
+			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = @ServerThread and convert(argument using utf8) like '%isolation level%' and argument not like 'select%' order by event_time;", new { m_connection.ServerThread });
+			var lastIsolationLevelQuery = results.Last();
 
 			Assert.Contains(expectedTransactionIsolationLevel.ToLower(), lastIsolationLevelQuery.ToLower());
 		}
@@ -95,22 +89,13 @@ namespace SideBySide
 		public void DbConnectionTransactionCommand(IsolationLevel inputIsolationLevel, string expectedTransactionIsolationLevel)
 		{
 			DbConnection connection = m_connection;
-			m_connection.Execute(@"set global log_output = 'table';");
-			m_connection.Execute(@"set global general_log = 1;");
+			connection.Execute(@"set global log_output = 'table';");
+			connection.Execute(@"set global general_log = 1;");
 			using (var trans = connection.BeginTransaction(inputIsolationLevel))
-			{
 				trans.Commit();
-			}
 
-			m_connection.Execute(@"set global general_log = 0;");
-			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = {m_connection.ServerThread} order by event_time desc limit 10;");
-			var lastStartTransactionQuery = results.First(x => x.ToLower().Contains("start"));
-
-			if (IsMySqlAndVersionLessThan57(m_connection.ServerVersion))
-			{
-				Assert.Contains("start transaction", lastStartTransactionQuery.ToLower());
-				return;
-			}
+			var results = connection.Query<string>($"select convert(argument USING utf8) from mysql.general_log where thread_id = @ServerThread and convert(argument using utf8) like '%start transaction%' and argument not like 'select%' order by event_time;", new { m_connection.ServerThread });
+			var lastStartTransactionQuery = results.Last();
 
 			Assert.Contains(expectedTransactionIsolationLevel.ToLower(), lastStartTransactionQuery.ToLower());
 		}
@@ -381,20 +366,6 @@ namespace SideBySide
 			Assert.Equal(new int[0], results);
 		}
 #endif
-
-		private bool IsMySqlAndVersionLessThan57(string currentVersionStr)
-		{
-			var version = new Version("5.7");
-			Version currentVersion = null;
-
-			if (Version.TryParse(currentVersionStr, out currentVersion))
-			{
-				var result = version.CompareTo(currentVersion);
-				return result > 0;
-			}
-
-			return false;
-		}
 
 		readonly TransactionFixture m_database;
 		readonly MySqlConnection m_connection;
