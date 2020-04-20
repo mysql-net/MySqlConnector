1cae060f3cecc629c768f5a696644d21a4128cd7
Bradley Grainger <bgrainger@gmail.com>
2020-04-20 13:51:26 -0700

    Move conformance tests to a template.

--- .ci/server/init.sql
+++ .ci/server/init.sql
@@ -3,4 +3,4 @@ CREATE USER 'no_password'@'localhost';
 CREATE USER 'no_password'@'172.17.0.1';
 CREATE USER 'ssltest'@'%' IDENTIFIED BY 'test' /*!50706 REQUIRE SSL */; GRANT ALL PRIVILEGES ON *.* TO 'ssltest'@'%';
 SET GLOBAL max_allowed_packet=104857600;
-
+CREATE SCHEMA conformance COLLATE utf8mb4_bin;
