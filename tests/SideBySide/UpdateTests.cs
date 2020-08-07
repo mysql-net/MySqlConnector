1bb0ebaa420f74fa21528aefe24b0e009140d356
Bradley Grainger <bgrainger@gmail.com>
2020-08-07 07:00:26 -0700

    Update several NuGet packages.

--- tests/SideBySide/UpdateTests.cs
+++ tests/SideBySide/UpdateTests.cs
@@ -143,62 +143,24 @@ insert into update_rows_dapper_async (value) VALUES (1), (2), (1), (4);
 		}
 
 		[Fact]
-		public async Task UpdateDapperNoColumnsWereSelected()
+		public void UpdateFieldCount()
 		{
-			await m_database.Connection.ExecuteAsync(@"drop table if exists update_station;
-create table update_station (
-	SID bigint unsigned,
-	name text,
-	stationType_SID bigint unsigned not null,
-	geoPosition_SID bigint unsigned,
-	service_start datetime,
-	service_end datetime,
-	deleted boolean,
-	created_on datetime not null,
-	externalWebsite text,
-	externalTitle text
-);
-insert into update_station values(1, 'name', 2, null, null, null, false, '2016-09-07 06:28:00', 'https://github.com/mysql-net/MySqlConnector/issues/44', 'Issue #44');
-").ConfigureAwait(false);
-
-			var queryString = @"UPDATE update_station SET name=@name,stationType_SID=@stationType_SID,geoPosition_SID=@geoPosition_SID,service_start=@service_start,service_end=@service_end,deleted=@deleted,created_on=@created_on,externalWebsite=@externalWebsite,externalTitle=@externalTitle WHERE SID=@SID";
-			var station = new Station
+			using (var cmd = m_database.Connection.CreateCommand())
 			{
-				SID = 1,
-				name = "new name",
-				stationType_SID = 3,
-				geoPosition_SID = null,
-				service_start = new(2016, 1, 1),
-				service_end = new(2017, 12, 31),
-				deleted = true,
-				created_on = new(2000, 1, 1),
-				externalWebsite = null,
-				externalTitle = null,
-			};
-
-			try
-			{
-				await m_database.Connection.QueryAsync<Station>(queryString, station).ConfigureAwait(false);
-				Assert.True(false, "Should throw InvalidOperationException");
+				cmd.CommandText = @"drop table if exists update_rows_reader;
+create table update_rows_reader(id integer not null primary key auto_increment, value text not null);
+insert into update_rows_reader (value) VALUES ('one'), ('two'), ('one'), ('four');
+";
+				cmd.ExecuteNonQuery();
 			}
-			catch (InvalidOperationException ex)
-			{
-				Assert.Equal("No columns were selected", ex.Message);
-			}
-		}
 
-		public class Station
-		{
-			public ulong? SID { get; set; }
-			public string name { get; set; }
-			public ulong stationType_SID { get; set; }
-			public ulong? geoPosition_SID { get; set; }
-			public DateTime? service_start { get; set; }
-			public DateTime? service_end { get; set; }
-			public bool? deleted { get; set; }
-			public DateTime created_on { get; set; }
-			public string externalWebsite { get; set; }
-			public string externalTitle { get; set; }
+			using (var cmd = new MySqlCommand(@"UPDATE update_rows_reader SET value = 'three' WHERE id = 3;", m_database.Connection))
+			using (var reader = cmd.ExecuteReader())
+			{
+				Assert.Equal(0, reader.FieldCount);
+				Assert.False(reader.HasRows);
+				Assert.False(reader.Read());
+			}
 		}
 
 		readonly DatabaseFixture m_database;
