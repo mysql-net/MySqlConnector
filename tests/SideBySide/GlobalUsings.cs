f1af9a620b37a67701d9722b341166a495331a4f
Bradley Grainger <bgrainger@gmail.com>
2021-10-01 14:22:33 -0700

    Move global usings to project file.
    Signed-off-by: Bradley Grainger <bgrainger@gmail.com>

--- tests/SideBySide/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-global using System;
-global using System.Data;
-global using Dapper;
-#if BASELINE
-global using MySql.Data.MySqlClient;
-#else
-global using MySqlConnector;
-#endif
-global using Xunit;
