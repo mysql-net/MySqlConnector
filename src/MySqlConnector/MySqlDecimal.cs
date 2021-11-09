5c712bc4238bea06c910a1b953508092f95722f7
sumit kumar <kusu@microsoft.com>
2021-11-08 19:19:54 -0800

    fixed identation
    Signed-off-by: sumit kumar <kusu@microsoft.com>

--- src/MySqlConnector/MySqlDecimal.cs
+++ src/MySqlConnector/MySqlDecimal.cs
@@ -6,28 +6,28 @@ namespace MySqlConnector;
 
 public readonly struct MySqlDecimal
 {
-private readonly string value;
+	private readonly string value;
 
-internal MySqlDecimal(string val)
-{
-	string regexWithDecimal = @"^-?([1-9][0-9]*|0)(\.[0-9]+)?$";
-	string regexWithOutDecimal = @"^-?([1-9][0-9]*|0)$";
-	var matchWithDecimal = Regex.Match(val, regexWithDecimal, RegexOptions.IgnoreCase);
-	var matchWithOutDecimal = Regex.Match(val, regexWithOutDecimal, RegexOptions.IgnoreCase);
-
-	if (!(matchWithDecimal.Success && val.Length <= 66) && !(matchWithOutDecimal.Success && val.Length <= 67))
+	internal MySqlDecimal(string val)
 	{
-		throw new MySqlConversionException("Value is too large for Conversion or Format is wrong.");
+		string regexWithDecimal = @"^-?([1-9][0-9]*|0)(\.[0-9]+)?$";
+		string regexWithOutDecimal = @"^-?([1-9][0-9]*|0)$";
+		var matchWithDecimal = Regex.Match(val, regexWithDecimal, RegexOptions.IgnoreCase);
+		var matchWithOutDecimal = Regex.Match(val, regexWithOutDecimal, RegexOptions.IgnoreCase);
+
+		if (!(matchWithDecimal.Success && val.Length <= 66) && !(matchWithOutDecimal.Success && val.Length <= 67))
+		{
+			throw new MySqlConversionException("Value is too large for Conversion or Format is wrong.");
+		}
+		value = val;
 	}
-	value = val;
-}
 
-public decimal Value => Convert.ToDecimal(value, CultureInfo.InvariantCulture);
+	public decimal Value => Convert.ToDecimal(value, CultureInfo.InvariantCulture);
 
-public double ToDouble()
-{
-	return Double.Parse(value, CultureInfo.InvariantCulture);
-}
+	public double ToDouble()
+	{
+		return Double.Parse(value, CultureInfo.InvariantCulture);
+	}
 
-public override string ToString() => value;
+	public override string ToString() => value;
 }
