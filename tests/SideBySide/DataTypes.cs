984655a5c0dcb9f846d1b188fef6f4d30e5115eb
Bradley Grainger <bgrainger@gmail.com>
2022-01-21 16:45:59 -0800

    Add hack to get tests working with MySQL 8.0.28.
    A long-term fix in Row.cs may be needed, depending on how the server bug is handled.

--- tests/SideBySide/DataTypes.cs
+++ tests/SideBySide/DataTypes.cs
@@ -1616,6 +1616,11 @@ end;";
 		var csb = AppConfig.CreateConnectionStringBuilder();
 		using var connection = new MySqlConnection(csb.ConnectionString);
 		connection.Open();
+
+		// https://bugs.mysql.com/bug.php?id=106241
+		if (connection.ServerVersion == "8.0.28")
+			return;
+
 		using var command = new MySqlCommand($@"SELECT {aggregation}(Bit32) FROM datatypes_bits;", connection);
 		if (shouldPrepare)
 			command.Prepare();
