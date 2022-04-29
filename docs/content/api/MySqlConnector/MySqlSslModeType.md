bcf4c067af5ad2bd96b76b061b84948563bc8373
Bradley Grainger <bgrainger@gmail.com>
2022-04-29 11:16:15 -0700

    Add MySqlSslMode.Disabled. Fixes #1168

--- docs/content/api/MySqlConnector/MySqlSslModeType.md
+++ docs/content/api/MySqlConnector/MySqlSslModeType.md
@@ -15,6 +15,7 @@ public enum MySqlSslMode
 | name | value | description |
 | --- | --- | --- |
 | None | `0` | Do not use SSL. |
+| Disabled | `0` | Do not use SSL. This is the same as None. |
 | Preferred | `1` | Use SSL if the server supports it. |
 | Required | `2` | Always use SSL. Deny connection if server does not support SSL. |
 | VerifyCA | `3` | Always use SSL. Validate the Certificate Authority but tolerate name mismatch. |
--- src/MySqlConnector/MySqlSslMode.cs
+++ src/MySqlConnector/MySqlSslMode.cs
@@ -3,12 +3,20 @@ namespace MySqlConnector;
 /// <summary>
 /// SSL connection options.
 /// </summary>
+#pragma warning disable CA1027 // not a [Flags] enum
 public enum MySqlSslMode
 {
 	/// <summary>
 	/// Do not use SSL.
 	/// </summary>
 	None,
+
+	/// <summary>
+	/// Do not use SSL. This is the same as <see cref="None"/>.
+	/// </summary>
+#pragma warning disable CA1069 // Enum values should not be duplicated
+	Disabled = 0,
+#pragma warning restore CA1069 // Enum values should not be duplicated
 
 	/// <summary>
 	/// Use SSL if the server supports it.
