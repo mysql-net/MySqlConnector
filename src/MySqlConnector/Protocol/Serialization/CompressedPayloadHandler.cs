6a6fe35bdb704f51ab2a75ceae07717f1e4db1e1
Bradley Grainger <bgrainger@gmail.com>
2022-10-18 09:30:27 -0700

    Eliminate ContinueWith.
    'await' should just be used directly instead of abstracting it behind a utility method.

--- src/MySqlConnector/Protocol/Serialization/CompressedPayloadHandler.cs
+++ src/MySqlConnector/Protocol/Serialization/CompressedPayloadHandler.cs
@@ -39,167 +39,157 @@ internal sealed class CompressedPayloadHandler : IPayloadHandler
 		return ProtocolUtility.ReadPayloadAsync(m_bufferedByteReader, compressedByteHandler, static () => -1, cache, protocolErrorBehavior, ioBehavior);
 	}
 
-	public ValueTask<int> WritePayloadAsync(ReadOnlyMemory<byte> payload, IOBehavior ioBehavior)
+	public async ValueTask<int> WritePayloadAsync(ReadOnlyMemory<byte> payload, IOBehavior ioBehavior)
 	{
 		// break the payload up into (possibly more than one) uncompressed packets
-		return ProtocolUtility.WritePayloadAsync(m_uncompressedStreamByteHandler!, GetNextUncompressedSequenceNumber, payload, ioBehavior).ContinueWith(_ =>
-		{
-			if (m_uncompressedStream!.Length == 0)
-				return default;
+		await ProtocolUtility.WritePayloadAsync(m_uncompressedStreamByteHandler!, GetNextUncompressedSequenceNumber, payload, ioBehavior).ConfigureAwait(false);
 
-			if (!m_uncompressedStream.TryGetBuffer(out var uncompressedData))
-				throw new InvalidOperationException("Couldn't get uncompressed stream buffer.");
+		if (m_uncompressedStream!.Length == 0)
+			return default;
 
-			return CompressAndWrite(uncompressedData, ioBehavior)
-				.ContinueWith(__ =>
-				{
-					// reset the uncompressed stream to accept more data
-					m_uncompressedStream.SetLength(0);
-					return default(ValueTask<int>);
-				});
-		});
+		if (!m_uncompressedStream.TryGetBuffer(out var uncompressedData))
+			throw new InvalidOperationException("Couldn't get uncompressed stream buffer.");
+
+		await CompressAndWrite(uncompressedData, ioBehavior).ConfigureAwait(false);
+
+		// reset the uncompressed stream to accept more data
+		m_uncompressedStream.SetLength(0);
+		return default;
 	}
 
-	private ValueTask<int> ReadBytesAsync(Memory<byte> buffer, ProtocolErrorBehavior protocolErrorBehavior, IOBehavior ioBehavior)
+	private async ValueTask<int> ReadBytesAsync(Memory<byte> buffer, ProtocolErrorBehavior protocolErrorBehavior, IOBehavior ioBehavior)
 	{
 		// satisfy the read from cache if possible
+		int bytesToRead;
 		if (m_remainingData.Count > 0)
 		{
-			var bytesToRead = Math.Min(m_remainingData.Count, buffer.Length);
+			bytesToRead = Math.Min(m_remainingData.Count, buffer.Length);
 			m_remainingData.AsSpan(0, bytesToRead).CopyTo(buffer.Span);
 			m_remainingData = m_remainingData.Slice(bytesToRead);
-			return new ValueTask<int>(bytesToRead);
+			return bytesToRead;
 		}
 
 		// read the compressed header (seven bytes)
-		return m_compressedBufferedByteReader.ReadBytesAsync(m_byteHandler!, 7, ioBehavior)
-			.ContinueWith(headerReadBytes =>
-			{
-				if (headerReadBytes.Count < 7)
-				{
-					return protocolErrorBehavior == ProtocolErrorBehavior.Ignore ?
-						default :
-						ValueTaskExtensions.FromException<int>(new EndOfStreamException("Wanted to read 7 bytes but only read {0} when reading compressed packet header".FormatInvariant(headerReadBytes.Count)));
-				}
+		var headerReadBytes = await m_compressedBufferedByteReader.ReadBytesAsync(m_byteHandler!, 7, ioBehavior).ConfigureAwait(false);
+		if (headerReadBytes.Count < 7)
+		{
+			if (protocolErrorBehavior == ProtocolErrorBehavior.Ignore)
+				return default;
+			throw new EndOfStreamException("Wanted to read 7 bytes but only read {0} when reading compressed packet header".FormatInvariant(headerReadBytes.Count));
+		}
 
-				var payloadLength = (int) SerializationUtility.ReadUInt32(headerReadBytes.AsSpan(0, 3));
-				var packetSequenceNumber = headerReadBytes.Array![headerReadBytes.Offset + 3];
-				var uncompressedLength = (int) SerializationUtility.ReadUInt32(headerReadBytes.AsSpan(4, 3));
+		var payloadLength = (int) SerializationUtility.ReadUInt32(headerReadBytes.AsSpan(0, 3));
+		var packetSequenceNumber = headerReadBytes.Array![headerReadBytes.Offset + 3];
+		var uncompressedLength = (int) SerializationUtility.ReadUInt32(headerReadBytes.AsSpan(4, 3));
 
-				// verify the compressed packet sequence number
-				var expectedSequenceNumber = GetNextCompressedSequenceNumber();
-				if (packetSequenceNumber != expectedSequenceNumber)
-				{
-					if (protocolErrorBehavior == ProtocolErrorBehavior.Ignore)
-						return default;
+		// verify the compressed packet sequence number
+		var expectedSequenceNumber = GetNextCompressedSequenceNumber();
+		if (packetSequenceNumber != expectedSequenceNumber)
+		{
+			if (protocolErrorBehavior == ProtocolErrorBehavior.Ignore)
+				return default;
+			throw MySqlProtocolException.CreateForPacketOutOfOrder(expectedSequenceNumber, packetSequenceNumber);
+		}
 
-					var exception = MySqlProtocolException.CreateForPacketOutOfOrder(expectedSequenceNumber, packetSequenceNumber);
-					return ValueTaskExtensions.FromException<int>(exception);
-				}
+		// MySQL protocol resets the uncompressed sequence number back to the sequence number of this compressed packet.
+		// This isn't in the documentation, but the code explicitly notes that uncompressed packets are modified by compression:
+		//  - https://github.com/mysql/mysql-server/blob/c28e258157f39f25e044bb72e8bae1ff00989a3d/sql/net_serv.cc#L276
+		//  - https://github.com/mysql/mysql-server/blob/c28e258157f39f25e044bb72e8bae1ff00989a3d/sql/net_serv.cc#L225-L227
+		if (!m_isContinuationPacket)
+			m_uncompressedSequenceNumber = packetSequenceNumber;
 
-				// MySQL protocol resets the uncompressed sequence number back to the sequence number of this compressed packet.
-				// This isn't in the documentation, but the code explicitly notes that uncompressed packets are modified by compression:
-				//  - https://github.com/mysql/mysql-server/blob/c28e258157f39f25e044bb72e8bae1ff00989a3d/sql/net_serv.cc#L276
-				//  - https://github.com/mysql/mysql-server/blob/c28e258157f39f25e044bb72e8bae1ff00989a3d/sql/net_serv.cc#L225-L227
-				if (!m_isContinuationPacket)
-					m_uncompressedSequenceNumber = packetSequenceNumber;
+		// except this doesn't happen when uncompressed packets need to be broken up across multiple compressed packets
+		m_isContinuationPacket = payloadLength == ProtocolUtility.MaxPacketSize || uncompressedLength == ProtocolUtility.MaxPacketSize;
 
-				// except this doesn't happen when uncompressed packets need to be broken up across multiple compressed packets
-				m_isContinuationPacket = payloadLength == ProtocolUtility.MaxPacketSize || uncompressedLength == ProtocolUtility.MaxPacketSize;
+		var payloadReadBytes = await m_compressedBufferedByteReader.ReadBytesAsync(m_byteHandler!, payloadLength, ioBehavior).ConfigureAwait(false);
+		if (payloadReadBytes.Count < payloadLength)
+		{
+			if (protocolErrorBehavior == ProtocolErrorBehavior.Ignore)
+				return default;
+			throw new EndOfStreamException("Wanted to read {0} bytes but only read {1} when reading compressed payload".FormatInvariant(payloadLength, payloadReadBytes.Count));
+		}
 
-				return m_compressedBufferedByteReader.ReadBytesAsync(m_byteHandler!, payloadLength, ioBehavior)
-					.ContinueWith(payloadReadBytes =>
-					{
-						if (payloadReadBytes.Count < payloadLength)
-						{
-							return protocolErrorBehavior == ProtocolErrorBehavior.Ignore ?
-								default :
-								ValueTaskExtensions.FromException<int>(new EndOfStreamException("Wanted to read {0} bytes but only read {1} when reading compressed payload".FormatInvariant(payloadLength, payloadReadBytes.Count)));
-						}
-
-						if (uncompressedLength == 0)
-						{
-							// data is uncompressed
-							m_remainingData = payloadReadBytes;
-						}
-						else
-						{
+		if (uncompressedLength == 0)
+		{
+			// data is uncompressed
+			m_remainingData = payloadReadBytes;
+		}
+		else
+		{
 #if NET6_0_OR_GREATER
-							var uncompressedData = new byte[uncompressedLength];
-							using var compressedStream = new MemoryStream(payloadReadBytes.Array!, payloadReadBytes.Offset, payloadReadBytes.Count);
-							using var decompressingStream = new ZLibStream(compressedStream, CompressionMode.Decompress);
+			var uncompressedData = new byte[uncompressedLength];
+			using var compressedStream = new MemoryStream(payloadReadBytes.Array!, payloadReadBytes.Offset, payloadReadBytes.Count);
+			using var decompressingStream = new ZLibStream(compressedStream, CompressionMode.Decompress);
 #if NET7_0_OR_GREATER
-							var totalBytesRead = decompressingStream.ReadAtLeast(uncompressedData, uncompressedLength, throwOnEndOfStream: false);
+			var totalBytesRead = decompressingStream.ReadAtLeast(uncompressedData, uncompressedLength, throwOnEndOfStream: false);
 #else
-							int bytesRead, totalBytesRead = 0;
-							do
-							{
-								bytesRead = decompressingStream.Read(uncompressedData, totalBytesRead, uncompressedLength - totalBytesRead);
-								totalBytesRead += bytesRead;
-							} while (bytesRead > 0);
+			int bytesRead, totalBytesRead = 0;
+			do
+			{
+				bytesRead = decompressingStream.Read(uncompressedData, totalBytesRead, uncompressedLength - totalBytesRead);
+				totalBytesRead += bytesRead;
+			} while (bytesRead > 0);
 #endif
-							if (totalBytesRead != uncompressedLength && protocolErrorBehavior == ProtocolErrorBehavior.Throw)
-								return ValueTaskExtensions.FromException<int>(new InvalidOperationException("Expected to read {0:n0} uncompressed bytes but only read {1:n0}".FormatInvariant(uncompressedLength, totalBytesRead)));
-							m_remainingData = new(uncompressedData, 0, totalBytesRead);
+			if (totalBytesRead != uncompressedLength && protocolErrorBehavior == ProtocolErrorBehavior.Throw)
+				throw new InvalidOperationException("Expected to read {0:n0} uncompressed bytes but only read {1:n0}".FormatInvariant(uncompressedLength, totalBytesRead));
+			m_remainingData = new(uncompressedData, 0, totalBytesRead);
 #else
-							// check CMF (Compression Method and Flags) and FLG (Flags) bytes for expected values
-							var cmf = payloadReadBytes.Array![payloadReadBytes.Offset];
-							var flg = payloadReadBytes.Array[payloadReadBytes.Offset + 1];
-							if (cmf != 0x78 || ((flg & 0x20) == 0x20) || ((cmf * 256 + flg) % 31 != 0))
-							{
-								// CMF = 0x78: 32K Window Size + deflate compression
-								// FLG & 0x20: has preset dictionary (not supported)
-								// CMF*256+FLG is a multiple of 31: header checksum
-								return protocolErrorBehavior == ProtocolErrorBehavior.Ignore ?
-									default :
-									ValueTaskExtensions.FromException<int>(new NotSupportedException("Unsupported zlib header: {0:X2}{1:X2}".FormatInvariant(cmf, flg)));
-							}
+			// check CMF (Compression Method and Flags) and FLG (Flags) bytes for expected values
+			var cmf = payloadReadBytes.Array![payloadReadBytes.Offset];
+			var flg = payloadReadBytes.Array[payloadReadBytes.Offset + 1];
+			if (cmf != 0x78 || ((flg & 0x20) == 0x20) || ((cmf * 256 + flg) % 31 != 0))
+			{
+				// CMF = 0x78: 32K Window Size + deflate compression
+				// FLG & 0x20: has preset dictionary (not supported)
+				// CMF*256+FLG is a multiple of 31: header checksum
+				if (protocolErrorBehavior == ProtocolErrorBehavior.Ignore)
+					return default;
+				throw new NotSupportedException("Unsupported zlib header: {0:X2}{1:X2}".FormatInvariant(cmf, flg));
+			}
 
-							// zlib format (https://www.ietf.org/rfc/rfc1950.txt) is: [two header bytes] [deflate-compressed data] [four-byte checksum]
-							// .NET implements the middle part with DeflateStream; need to handle header and checksum explicitly
-							const int headerSize = 2;
-							const int checksumSize = 4;
-							var uncompressedData = new byte[uncompressedLength];
-							using var compressedStream = new MemoryStream(payloadReadBytes.Array, payloadReadBytes.Offset + headerSize, payloadReadBytes.Count - headerSize - checksumSize);
-							using var decompressingStream = new DeflateStream(compressedStream, CompressionMode.Decompress);
-							int bytesRead, totalBytesRead = 0;
-							do
-							{
-								bytesRead = decompressingStream.Read(uncompressedData, totalBytesRead, uncompressedLength - totalBytesRead);
-								totalBytesRead += bytesRead;
-							} while (bytesRead > 0);
-							if (totalBytesRead != uncompressedLength && protocolErrorBehavior == ProtocolErrorBehavior.Throw)
-								return ValueTaskExtensions.FromException<int>(new InvalidOperationException("Expected to read {0:n0} uncompressed bytes but only read {1:n0}".FormatInvariant(uncompressedLength, totalBytesRead)));
-							m_remainingData = new(uncompressedData, 0, totalBytesRead);
+			// zlib format (https://www.ietf.org/rfc/rfc1950.txt) is: [two header bytes] [deflate-compressed data] [four-byte checksum]
+			// .NET implements the middle part with DeflateStream; need to handle header and checksum explicitly
+			const int headerSize = 2;
+			const int checksumSize = 4;
+			var uncompressedData = new byte[uncompressedLength];
+			using var compressedStream = new MemoryStream(payloadReadBytes.Array, payloadReadBytes.Offset + headerSize, payloadReadBytes.Count - headerSize - checksumSize);
+			using var decompressingStream = new DeflateStream(compressedStream, CompressionMode.Decompress);
+			int bytesRead, totalBytesRead = 0;
+			do
+			{
+				bytesRead = decompressingStream.Read(uncompressedData, totalBytesRead, uncompressedLength - totalBytesRead);
+				totalBytesRead += bytesRead;
+			} while (bytesRead > 0);
+			if (totalBytesRead != uncompressedLength && protocolErrorBehavior == ProtocolErrorBehavior.Throw)
+				throw new InvalidOperationException("Expected to read {0:n0} uncompressed bytes but only read {1:n0}".FormatInvariant(uncompressedLength, totalBytesRead));
+			m_remainingData = new(uncompressedData, 0, totalBytesRead);
 
-							var checksum = Adler32.Calculate(uncompressedData.AsSpan(0, totalBytesRead));
+			var checksum = Adler32.Calculate(uncompressedData.AsSpan(0, totalBytesRead));
 
-							var adlerStartOffset = payloadReadBytes.Offset + payloadReadBytes.Count - 4;
-							if (payloadReadBytes.Array[adlerStartOffset + 0] != ((checksum >> 24) & 0xFF) ||
-								payloadReadBytes.Array[adlerStartOffset + 1] != ((checksum >> 16) & 0xFF) ||
-								payloadReadBytes.Array[adlerStartOffset + 2] != ((checksum >> 8) & 0xFF) ||
-								payloadReadBytes.Array[adlerStartOffset + 3] != (checksum & 0xFF))
-							{
-								return protocolErrorBehavior == ProtocolErrorBehavior.Ignore ?
-									default :
-									ValueTaskExtensions.FromException<int>(new NotSupportedException("Invalid Adler-32 checksum of uncompressed data."));
-							}
+			var adlerStartOffset = payloadReadBytes.Offset + payloadReadBytes.Count - 4;
+			if (payloadReadBytes.Array[adlerStartOffset + 0] != ((checksum >> 24) & 0xFF) ||
+				payloadReadBytes.Array[adlerStartOffset + 1] != ((checksum >> 16) & 0xFF) ||
+				payloadReadBytes.Array[adlerStartOffset + 2] != ((checksum >> 8) & 0xFF) ||
+				payloadReadBytes.Array[adlerStartOffset + 3] != (checksum & 0xFF))
+			{
+				if (protocolErrorBehavior == ProtocolErrorBehavior.Ignore)
+					return default;
+				throw new NotSupportedException("Invalid Adler-32 checksum of uncompressed data.");
+			}
 #endif
-						}
+		}
 
-						var bytesToRead = Math.Min(m_remainingData.Count, buffer.Length);
-						m_remainingData.AsSpan(0, bytesToRead).CopyTo(buffer.Span);
-						m_remainingData = m_remainingData.Slice(bytesToRead);
-						return new ValueTask<int>(bytesToRead);
-					});
-			});
+		bytesToRead = Math.Min(m_remainingData.Count, buffer.Length);
+		m_remainingData.AsSpan(0, bytesToRead).CopyTo(buffer.Span);
+		m_remainingData = m_remainingData.Slice(bytesToRead);
+		return bytesToRead;
 	}
 
 	private byte GetNextCompressedSequenceNumber() => m_compressedSequenceNumber++;
 
 	private int GetNextUncompressedSequenceNumber() => m_uncompressedSequenceNumber++;
 
-	private ValueTask<int> CompressAndWrite(ArraySegment<byte> remainingUncompressedData, IOBehavior ioBehavior)
+	private async ValueTask<int> CompressAndWrite(ArraySegment<byte> remainingUncompressedData, IOBehavior ioBehavior)
 	{
 		var remainingUncompressedBytes = Math.Min(remainingUncompressedData.Count, ProtocolUtility.MaxPacketSize);
 
@@ -248,9 +238,9 @@ internal sealed class CompressedPayloadHandler : IPayloadHandler
 		Buffer.BlockCopy(compressedData.Array!, compressedData.Offset, buffer, 7, compressedData.Count);
 
 		remainingUncompressedData = remainingUncompressedData.Slice(remainingUncompressedBytes);
-		return m_byteHandler!.WriteBytesAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), ioBehavior)
-			.ContinueWith(_ => remainingUncompressedData.Count == 0 ? default :
-				CompressAndWrite(remainingUncompressedData, ioBehavior));
+		await m_byteHandler!.WriteBytesAsync(new ArraySegment<byte>(buffer, 0, buffer.Length), ioBehavior).ConfigureAwait(false);
+		return remainingUncompressedData.Count == 0 ? default :
+			await CompressAndWrite(remainingUncompressedData, ioBehavior).ConfigureAwait(false);
 	}
 
 	// CompressedByteHandler implements IByteHandler and delegates reading bytes back to the CompressedPayloadHandler class.
--- src/MySqlConnector/Utilities/ValueTaskExtensions.cs
+++ src/MySqlConnector/Utilities/ValueTaskExtensions.cs
@@ -2,7 +2,5 @@ namespace MySqlConnector.Utilities;
 
 internal static class ValueTaskExtensions
 {
-	public static async ValueTask<TResult> ContinueWith<T, TResult>(this ValueTask<T> valueTask, Func<T, ValueTask<TResult>> continuation) => await continuation(await valueTask.ConfigureAwait(false)).ConfigureAwait(false);
-
 	public static ValueTask<T> FromException<T>(Exception exception) => new ValueTask<T>(Task.FromException<T>(exception));
 }
