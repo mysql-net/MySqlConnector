mysql-net/MySqlConnector

commit 0bb5885895be202f0970aa7515164b7ca80745c5

Use C# 11 list patterns.
Add IndexRange package to support this on older frameworks, then adopt index and range notation when possible.

src/MySqlConnector/Utilities/Utility.cs
@@ -139,7 +139,7 @@ internal static class Utility
 #endif
 
 #if NETCOREAPP2_1_OR_GREATER || NETSTANDARD2_1_OR_GREATER
-		var keyChars = key.AsSpan().Slice(keyStartIndex, keyEndIndex - keyStartIndex);
+		var keyChars = key.AsSpan()[keyStartIndex..keyEndIndex];
 		var bufferLength = keyChars.Length / 4 * 3;
 		byte[]? buffer = null;
 		Span<byte> bufferBytes = bufferLength <= 1024 ? stackalloc byte[bufferLength] : default;
@@ -154,11 +154,11 @@ internal static class Utility
 				throw new FormatException("The input is not a valid Base-64 string.");
 #if NET5_0_OR_GREATER
 			if (isPrivate)
-				rsa.ImportRSAPrivateKey(bufferBytes.Slice(0, bytesWritten), out var _);
+				rsa.ImportRSAPrivateKey(bufferBytes[..bytesWritten], out var _);
 			else
-				rsa.ImportSubjectPublicKeyInfo(bufferBytes.Slice(0, bytesWritten), out var _);
+				rsa.ImportSubjectPublicKeyInfo(bufferBytes[..bytesWritten], out var _);
 #else
-			return GetRsaParameters(bufferBytes.Slice(0, bytesWritten), isPrivate);
+			return GetRsaParameters(bufferBytes[..bytesWritten], isPrivate);
 #endif
 		}
 		finally
@@ -418,10 +418,10 @@ internal static class Utility
 
 		// parse (optional) leading minus sign
 		var isNegative = false;
-		if (value.Length > 0 && value[0] == 0x2D)
+		if (value is [ 0x2D, .. ])
 		{
 			isNegative = true;
-			value = value.Slice(1);
+			value = value[1..];
 		}
 
 		// parse hours (0-838)
@@ -436,7 +436,7 @@ internal static class Utility
 			goto InvalidTimeSpan;
 		if (value.Length < 3 || value[2] != 58)
 			goto InvalidTimeSpan;
-		value = value.Slice(3);
+		value = value[3..];
 
 		// parse seconds (0-59)
 		if (!Utf8Parser.TryParse(value, out int seconds, out bytesConsumed) || bytesConsumed != 2 || seconds < 0 || seconds > 59)
@@ -451,7 +451,7 @@ internal static class Utility
 		{
 			if (value[2] != 46)
 				goto InvalidTimeSpan;
-			value = value.Slice(3);
+			value = value[3..];
 			if (!Utf8Parser.TryParse(value, out microseconds, out bytesConsumed) || bytesConsumed != value.Length || microseconds < 0 || microseconds > 999_999)
 				goto InvalidTimeSpan;
 			for (; bytesConsumed < 6; bytesConsumed++)
@@ -520,13 +520,13 @@ internal static class Utility
 
 	public static byte[] TrimZeroByte(byte[] value)
 	{
-		if (value[value.Length - 1] == 0)
+		if (value is [ .., 0 ])
 			Array.Resize(ref value, value.Length - 1);
 		return value;
 	}
 
 	public static ReadOnlySpan<byte> TrimZeroByte(ReadOnlySpan<byte> value) =>
-		value[value.Length - 1] == 0 ? value.Slice(0, value.Length - 1) : value;
+		value is [ .., 0 ] ? value[..^1] : value;
 
 #if NET45
 	public static bool TryGetBuffer(this MemoryStream memoryStream, out ArraySegment<byte> buffer)
@@ -692,13 +692,13 @@ internal static class Utility
 	private static bool TryReadAsnInteger(ReadOnlySpan<byte> data, out ReadOnlySpan<byte> number, out int bytesConsumed)
 	{
 		// integer tag is 2
-		if (data[0] != 0x02)
+		if (data is not [ 0x02, .. ])
 		{
 			number = default;
 			bytesConsumed = 0;
 			return false;
 		}
-		data = data.Slice(1);
+		data = data[1..];
 
 		// tag is followed by the length of the integer
 		if (!TryReadAsnLength(data, out var length, out var lengthBytesConsumed))
@@ -713,8 +713,8 @@ internal static class Utility
 		bytesConsumed = lengthBytesConsumed + length + 1;
 
 		// trim leading zero bytes
-		while (number.Length > 1 && number[0] == 0)
-			number = number.Slice(1);
+		while (number is [ 0, .. ])
+			number = number[1..];
 
 		return true;
 	}

.editorconfig
@@ -84,6 +84,8 @@ dotnet_diagnostic.CA5398.severity = none # Avoid hardcoded SslProtocols values.
 dotnet_diagnostic.SA1003.severity = none # Operator must not be followed by whitespace.
 dotnet_diagnostic.SA1008.severity = none # Opening parenthesis must not be preceded by a space.
 dotnet_diagnostic.SA1009.severity = none # Closing parenthesis must not be followed by a space.
+dotnet_diagnostic.SA1010.severity = silent # https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3503
+dotnet_diagnostic.SA1011.severity = silent # https://github.com/DotNetAnalyzers/StyleCopAnalyzers/issues/3503
 dotnet_diagnostic.SA1027.severity = none # Tabs must not be used.
 dotnet_diagnostic.SA1101.severity = none # Prefix local calls with this.
 dotnet_diagnostic.SA1116.severity = none # Split parameters should start on line after declaration.

src/MySqlConnector/Core/BatchedCommandPayloadCreator.cs
@@ -27,7 +27,7 @@ internal sealed class BatchedCommandPayloadCreator : ICommandPayloadCreator
 			var commandLength = writer.Position - position - padding.Length;
 			var span = writer.ArraySegment.AsSpan().Slice(position);
 			span[0] = 0xFE;
-			BinaryPrimitives.WriteUInt64LittleEndian(span.Slice(1), (ulong) commandLength);
+			BinaryPrimitives.WriteUInt64LittleEndian(span[1..], (ulong) commandLength);
 		} while (wroteCommand);
 
 		// remove the padding that was saved for the final command (which wasn't written)

src/MySqlConnector/Core/BinaryRow.cs
@@ -186,7 +186,7 @@ internal sealed class BinaryRow : Row
 			second = value[6];
 		}
 
-		var microseconds = value.Length <= 7 ? 0 : MemoryMarshal.Read<int>(value.Slice(7));
+		var microseconds = value.Length <= 7 ? 0 : MemoryMarshal.Read<int>(value[7..]);
 
 		try
 		{
@@ -209,11 +209,11 @@ internal sealed class BinaryRow : Row
 			return TimeSpan.Zero;
 
 		var isNegative = value[0];
-		var days = MemoryMarshal.Read<int>(value.Slice(1));
+		var days = MemoryMarshal.Read<int>(value[1..]);
 		var hours = (int) value[5];
 		var minutes = (int) value[6];
 		var seconds = (int) value[7];
-		var microseconds = value.Length == 8 ? 0 : MemoryMarshal.Read<int>(value.Slice(8));
+		var microseconds = value.Length == 8 ? 0 : MemoryMarshal.Read<int>(value[8..]);
 
 		if (isNegative != 0)
 		{

src/MySqlConnector/Core/Row.cs
@@ -135,7 +135,7 @@ internal abstract class Row
 		var offset = (int) dataOffset;
 		var lengthToCopy = Math.Max(0, Math.Min(m_dataLengths[ordinal] - offset, length));
 		if (lengthToCopy > 0)
-			m_data.Slice(m_dataOffsets[ordinal] + offset, lengthToCopy).Span.CopyTo(buffer.AsSpan().Slice(bufferOffset));
+			m_data.Slice(m_dataOffsets[ordinal] + offset, lengthToCopy).Span.CopyTo(buffer.AsSpan()[bufferOffset..]);
 		return lengthToCopy;
 	}
 
@@ -487,11 +487,11 @@ internal abstract class Row
 			goto InvalidDateTime;
 		if (value.Length < 5 || value[4] != 45)
 			goto InvalidDateTime;
-		if (!Utf8Parser.TryParse(value.Slice(5), out int month, out bytesConsumed) || bytesConsumed != 2)
+		if (!Utf8Parser.TryParse(value[5..], out int month, out bytesConsumed) || bytesConsumed != 2)
 			goto InvalidDateTime;
 		if (value.Length < 8 || value[7] != 45)
 			goto InvalidDateTime;
-		if (!Utf8Parser.TryParse(value.Slice(8), out int day, out bytesConsumed) || bytesConsumed != 2)
+		if (!Utf8Parser.TryParse(value[8..], out int day, out bytesConsumed) || bytesConsumed != 2)
 			goto InvalidDateTime;
 
 		if (year == 0 && month == 0 && day == 0)
@@ -515,15 +515,15 @@ internal abstract class Row
 		{
 			if (value[10] != 32)
 				goto InvalidDateTime;
-			if (!Utf8Parser.TryParse(value.Slice(11), out hour, out bytesConsumed) || bytesConsumed != 2)
+			if (!Utf8Parser.TryParse(value[11..], out hour, out bytesConsumed) || bytesConsumed != 2)
 				goto InvalidDateTime;
 			if (value.Length < 14 || value[13] != 58)
 				goto InvalidDateTime;
-			if (!Utf8Parser.TryParse(value.Slice(14), out minute, out bytesConsumed) || bytesConsumed != 2)
+			if (!Utf8Parser.TryParse(value[14..], out minute, out bytesConsumed) || bytesConsumed != 2)
 				goto InvalidDateTime;
 			if (value.Length < 17 || value[16] != 58)
 				goto InvalidDateTime;
-			if (!Utf8Parser.TryParse(value.Slice(17), out second, out bytesConsumed) || bytesConsumed != 2)
+			if (!Utf8Parser.TryParse(value[17..], out second, out bytesConsumed) || bytesConsumed != 2)
 				goto InvalidDateTime;
 
 			if (value.Length == 19)
@@ -535,7 +535,7 @@ internal abstract class Row
 				if (value[19] != 46)
 					goto InvalidDateTime;
 
-				if (!Utf8Parser.TryParse(value.Slice(20), out microseconds, out bytesConsumed) || bytesConsumed != value.Length - 20)
+				if (!Utf8Parser.TryParse(value[20..], out microseconds, out bytesConsumed) || bytesConsumed != value.Length - 20)
 					goto InvalidDateTime;
 				for (; bytesConsumed < 6; bytesConsumed++)
 					microseconds *= 10;

src/MySqlConnector/Core/ServerSession.cs
@@ -168,20 +168,20 @@ internal sealed class ServerSession
 				buffer[2] = 'L';
 				buffer[3] = 'L';
 				buffer[4] = ' ';
-				buffer = buffer.Slice(5);
+				buffer = buffer[5..];
 				state.commandText.AsSpan().CopyTo(buffer);
-				buffer = buffer.Slice(state.commandText.Length);
+				buffer = buffer[state.commandText.Length..];
 				buffer[0] = '(';
-				buffer = buffer.Slice(1);
+				buffer = buffer[1..];
 				if (state.parameterCount > 0)
 				{
 					buffer[0] = '?';
-					buffer = buffer.Slice(1);
+					buffer = buffer[1..];
 					for (var i = 1; i < state.parameterCount; i++)
 					{
 						buffer[0] = ',';
 						buffer[1] = '?';
-						buffer = buffer.Slice(2);
+						buffer = buffer[2..];
 					}
 				}
 				buffer[0] = ')';
@@ -238,7 +238,7 @@ internal sealed class ServerSession
 					payload = await ReceiveReplyAsync(ioBehavior, cancellationToken).ConfigureAwait(false);
 					var payloadLength = payload.Span.Length;
 					Utility.Resize(ref columnsAndParameters, columnsAndParametersSize + payloadLength);
-					payload.Span.CopyTo(columnsAndParameters.Array.AsSpan().Slice(columnsAndParametersSize));
+					payload.Span.CopyTo(columnsAndParameters.Array.AsSpan()[columnsAndParametersSize..]);
 					parameters[i] = ColumnDefinitionPayload.Create(new(columnsAndParameters, columnsAndParametersSize, payloadLength));
 					columnsAndParametersSize += payloadLength;
 				}
@@ -258,7 +258,7 @@ internal sealed class ServerSession
 					payload = await ReceiveReplyAsync(ioBehavior, cancellationToken).ConfigureAwait(false);
 					var payloadLength = payload.Span.Length;
 					Utility.Resize(ref columnsAndParameters, columnsAndParametersSize + payloadLength);
-					payload.Span.CopyTo(columnsAndParameters.Array.AsSpan().Slice(columnsAndParametersSize));
+					payload.Span.CopyTo(columnsAndParameters.Array.AsSpan()[columnsAndParametersSize..]);
 					columns[i] = ColumnDefinitionPayload.Create(new(columnsAndParameters, columnsAndParametersSize, payloadLength));
 					columnsAndParametersSize += payloadLength;
 				}
@@ -499,7 +499,7 @@ internal sealed class ServerSession
 
 						// second packet: SET NAMES query
 						m_pipelinedResetConnectionBytes[5] = (byte) m_setNamesPayload.Span.Length;
-						m_setNamesPayload.Span.CopyTo(m_pipelinedResetConnectionBytes.AsSpan().Slice(9));
+						m_setNamesPayload.Span.CopyTo(m_pipelinedResetConnectionBytes.AsSpan()[9..]);
 					}
 				}
 

src/MySqlConnector/Core/ServerVersion.cs
@@ -14,19 +14,19 @@ internal sealed class ServerVersion
 		var build = 0;
 		if (Utf8Parser.TryParse(versionString, out int major, out var bytesConsumed))
 		{
-			versionString = versionString.Slice(bytesConsumed);
-			if (versionString.Length > 0 && versionString[0] == 0x2E)
+			versionString = versionString[bytesConsumed..];
+			if (versionString is [ 0x2E, ..])
 			{
-				versionString = versionString.Slice(1);
+				versionString = versionString[1..];
 				if (Utf8Parser.TryParse(versionString, out minor, out bytesConsumed))
 				{
-					versionString = versionString.Slice(bytesConsumed);
-					if (versionString.Length > 0 && versionString[0] == 0x2E)
+					versionString = versionString[bytesConsumed..];
+					if (versionString is [ 0x2E, .. ])
 					{
-						versionString = versionString.Slice(1);
+						versionString = versionString[1..];
 						if (Utf8Parser.TryParse(versionString, out build, out bytesConsumed))
 						{
-							versionString = versionString.Slice(bytesConsumed);
+							versionString = versionString[bytesConsumed..];
 						}
 					}
 				}
@@ -36,9 +36,9 @@ internal sealed class ServerVersion
 		Version = new Version(major, minor, build);
 
 		// check for MariaDB version appended to a fake MySQL version
-		if (versionString.Length != 0 && versionString[0] == 0x2D)
+		if (versionString is [ 0x2D, .. ])
 		{
-			versionString = versionString.Slice(1);
+			versionString = versionString[1..];
 			ReadOnlySpan<byte> mariaDb = "-MariaDB";
 			var mariaDbIndex = versionString.IndexOf(mariaDb);
 			if (mariaDbIndex != -1)
@@ -46,11 +46,11 @@ internal sealed class ServerVersion
 				var totalBytesRead = 0;
 				if (Utf8Parser.TryParse(versionString, out major, out bytesConsumed) && versionString[bytesConsumed] == 0x2E)
 				{
-					versionString = versionString.Slice(bytesConsumed + 1);
+					versionString = versionString[(bytesConsumed + 1)..];
 					totalBytesRead += bytesConsumed + 1;
 					if (Utf8Parser.TryParse(versionString, out minor, out bytesConsumed) && versionString[bytesConsumed] == 0x2E)
 					{
-						versionString = versionString.Slice(bytesConsumed + 1);
+						versionString = versionString[(bytesConsumed + 1)..];
 						totalBytesRead += bytesConsumed + 1;
 						if (Utf8Parser.TryParse(versionString, out build, out bytesConsumed) && versionString[bytesConsumed] == 0x2D)
 						{

src/MySqlConnector/MySqlBulkCopy.cs
@@ -405,7 +405,7 @@ public sealed class MySqlBulkCopy
 
 					var inputIndex = 0;
 					var bytesWritten = 0;
-					while (outputIndex >= maxLength || !WriteValue(m_connection, values[valueIndex], ref inputIndex, ref utf8Encoder, buffer.AsSpan(0, maxLength).Slice(outputIndex), out bytesWritten))
+					while (outputIndex >= maxLength || !WriteValue(m_connection, values[valueIndex], ref inputIndex, ref utf8Encoder, buffer.AsSpan(0, maxLength)[outputIndex..], out bytesWritten))
 					{
 						var payload = new PayloadData(new ArraySegment<byte>(buffer, 0, outputIndex + bytesWritten));
 						await m_connection.Session.SendReplyAsync(payload, ioBehavior, cancellationToken).ConfigureAwait(false);
@@ -643,7 +643,7 @@ public sealed class MySqlBulkCopy
 
 					output[0] = (byte) '\\';
 					output[1] = (byte) value[inputIndex];
-					output = output.Slice(2);
+					output = output[2..];
 					bytesWritten += 2;
 					inputIndex++;
 				}
@@ -659,7 +659,7 @@ public sealed class MySqlBulkCopy
 					utf8Encoder.Convert(value.AsSpan(inputIndex, nextIndex - inputIndex), output, nextIndex == value.Length, out var charsUsed, out var bytesUsed, out var completed);
 
 					bytesWritten += bytesUsed;
-					output = output.Slice(bytesUsed);
+					output = output[bytesUsed..];
 					inputIndex += charsUsed;
 
 					if (!completed)
@@ -679,7 +679,7 @@ public sealed class MySqlBulkCopy
 				var by = value[inputIndex];
 				output[0] = hex[(by >> 4) & 0xF];
 				output[1] = hex[by & 0xF];
-				output = output.Slice(2);
+				output = output[2..];
 				bytesWritten += 2;
 			}
 

commit f7044cc97c68b14661fef8158e9917ed76c1cb2f

Fix bug decoding ASN integers. Fixes #1242
This introduces a regression introduced in 0bb5885895be202f0970aa7515164b7ca80745c5 for older TFMs.

src/MySqlConnector/Utilities/Utility.cs
@@ -653,7 +653,7 @@ internal static class Utility
 		bytesConsumed = lengthBytesConsumed + length + 1;
 
 		// trim leading zero bytes
-		while (number is [ 0, .. ])
+		while (number is [ 0, _, .. ])
 			number = number[1..];
 
 		return true;

tests/MySqlConnector.Tests/MySqlConnector.Tests.csproj
@@ -1,7 +1,7 @@
 <Project Sdk="Microsoft.NET.Sdk">
 
   <PropertyGroup Condition=" '$(Configuration)' != 'MySqlData' ">
-    <TargetFrameworks>net7.0</TargetFrameworks>
+    <TargetFrameworks>netcoreapp3.1;net7.0</TargetFrameworks>
   </PropertyGroup>
 
  <PropertyGroup Condition=" '$(Configuration)' == 'MySqlData' ">

tests/MySqlConnector.Tests/UtilityTests.cs
@@ -110,9 +110,13 @@ public class UtilityTests
 	[InlineData("pre", "post")]
 	public void DecodePublicKey(string pre, string post)
 	{
+#if NET5_0_OR_GREATER
 		using var rsa = RSA.Create();
 		Utility.LoadRsaParameters(pre + c_publicKey + post, rsa);
 		var parameters = rsa.ExportParameters(false);
+#else
+		var parameters = Utility.GetRsaParameters(pre + c_publicKey + post);
+#endif
 		Assert.Equal(s_modulus, parameters.Modulus);
 		Assert.Equal(s_exponent, parameters.Exponent);
 	}
@@ -124,9 +128,13 @@ public class UtilityTests
 	[InlineData("pre", "post")]
 	public void DecodePrivateKey(string pre, string post)
 	{
+#if NET5_0_OR_GREATER
 		using var rsa = RSA.Create();
 		Utility.LoadRsaParameters(pre + c_privateKey + post, rsa);
 		var parameters = rsa.ExportParameters(true);
+#else
+		var parameters = Utility.GetRsaParameters(pre + c_privateKey + post);
+#endif
 		Assert.Equal(s_modulus, parameters.Modulus);
 		Assert.Equal(s_exponent, parameters.Exponent);
 		Assert.Equal(s_d, parameters.D);