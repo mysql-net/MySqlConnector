2ecd74d0fa1c12cff6f5e338d677f8dac4b5ba5a
Bradley Grainger <bgrainger@gmail.com>
2023-06-01 23:03:13 -0700

    Use IUtf8SpanFormattable.
    See https://devblogs.microsoft.com/dotnet/announcing-dotnet-8-preview-4/#libraries-utf8-improvements.

--- src/MySqlConnector/MySqlParameter.cs
+++ src/MySqlConnector/MySqlParameter.cs
@@ -4,6 +4,9 @@ using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Numerics;
 using System.Text;
+#if NET8_0_OR_GREATER
+using System.Text.Unicode;
+#endif
 using MySqlConnector.Core;
 using MySqlConnector.Protocol.Serialization;
 using MySqlConnector.Utilities;
@@ -243,7 +246,12 @@ public sealed class MySqlParameter : DbParameter, IDbDataParameter, ICloneable
 		}
 		else if (Value is decimal decimalValue)
 		{
+#if NET8_0_OR_GREATER
+			decimalValue.TryFormat(writer.GetSpan(31), out var bytesWritten, default, CultureInfo.InvariantCulture);
+			writer.Advance(bytesWritten);
+#else
 			writer.WriteAscii(decimalValue.ToString(CultureInfo.InvariantCulture));
+#endif
 		}
 		else if (Value is short shortValue)
 		{
@@ -319,13 +327,23 @@ public sealed class MySqlParameter : DbParameter, IDbDataParameter, ICloneable
 		}
 		else if (Value is float floatValue)
 		{
+#if NET8_0_OR_GREATER
+			floatValue.TryFormat(writer.GetSpan(14), out var bytesWritten, "R", CultureInfo.InvariantCulture);
+			writer.Advance(bytesWritten);
+#else
 			// NOTE: Utf8Formatter doesn't support "R"
 			writer.WriteAscii(floatValue.ToString("R", CultureInfo.InvariantCulture));
+#endif
 		}
 		else if (Value is double doubleValue)
 		{
+#if NET8_0_OR_GREATER
+			doubleValue.TryFormat(writer.GetSpan(24), out var bytesWritten, "R", CultureInfo.InvariantCulture);
+			writer.Advance(bytesWritten);
+#else
 			// NOTE: Utf8Formatter doesn't support "R"
 			writer.WriteAscii(doubleValue.ToString("R", CultureInfo.InvariantCulture));
+#endif
 		}
 		else if (Value is BigInteger bigInteger)
 		{
@@ -339,12 +357,17 @@ public sealed class MySqlParameter : DbParameter, IDbDataParameter, ICloneable
 		{
 			if (mySqlDateTimeValue.IsValidDateTime)
 			{
+#if NET8_0_OR_GREATER
+				Utf8.TryWrite(writer.GetSpan(39), CultureInfo.InvariantCulture, $"timestamp('{mySqlDateTimeValue.GetDateTime():yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')", out var bytesWritten);
+				writer.Advance(bytesWritten);
+#else
 #if NET6_0_OR_GREATER
 				var str = string.Create(CultureInfo.InvariantCulture, stackalloc char[39], $"timestamp('{mySqlDateTimeValue.GetDateTime():yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')");
 #else
 				var str = FormattableString.Invariant($"timestamp('{mySqlDateTimeValue.GetDateTime():yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')");
 #endif
 				writer.WriteAscii(str);
+#endif
 			}
 			else
 			{
@@ -354,7 +377,12 @@ public sealed class MySqlParameter : DbParameter, IDbDataParameter, ICloneable
 #if NET6_0_OR_GREATER
 		else if (Value is DateOnly dateOnlyValue)
 		{
+#if NET8_0_OR_GREATER
+			Utf8.TryWrite(writer.GetSpan(23), CultureInfo.InvariantCulture, $"timestamp('{dateOnlyValue:yyyy'-'MM'-'dd}')", out var bytesWritten);
+			writer.Advance(bytesWritten);
+#else
 			writer.WriteAscii(string.Create(CultureInfo.InvariantCulture, stackalloc char[23], $"timestamp('{dateOnlyValue:yyyy'-'MM'-'dd}')"));
+#endif
 		}
 #endif
 		else if (Value is DateTime dateTimeValue)
@@ -364,27 +392,42 @@ public sealed class MySqlParameter : DbParameter, IDbDataParameter, ICloneable
 			else if ((options & StatementPreparerOptions.DateTimeLocal) != 0 && dateTimeValue.Kind == DateTimeKind.Utc)
 				throw new MySqlException($"DateTime.Kind must not be Utc when DateTimeKind setting is Local (parameter name: {ParameterName})");
 
+#if NET8_0_OR_GREATER
+			Utf8.TryWrite(writer.GetSpan(39), CultureInfo.InvariantCulture, $"timestamp('{dateTimeValue:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')", out var bytesWritten);
+			writer.Advance(bytesWritten);
+#else
 #if NET6_0_OR_GREATER
 			var str = string.Create(CultureInfo.InvariantCulture, stackalloc char[39], $"timestamp('{dateTimeValue:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')");
 #else
 			var str = FormattableString.Invariant($"timestamp('{dateTimeValue:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')");
 #endif
 			writer.WriteAscii(str);
+#endif
 		}
 		else if (Value is DateTimeOffset dateTimeOffsetValue)
 		{
 			// store as UTC as it will be read as such when deserialized from a timespan column
+#if NET8_0_OR_GREATER
+			Utf8.TryWrite(writer.GetSpan(39), CultureInfo.InvariantCulture, $"timestamp('{dateTimeOffsetValue.UtcDateTime:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')", out var bytesWritten);
+			writer.Advance(bytesWritten);
+#else
 #if NET6_0_OR_GREATER
 			var str = string.Create(CultureInfo.InvariantCulture, stackalloc char[39], $"timestamp('{dateTimeOffsetValue.UtcDateTime:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')");
 #else
 			var str = FormattableString.Invariant($"timestamp('{dateTimeOffsetValue.UtcDateTime:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'ffffff}')");
 #endif
 			writer.WriteAscii(str);
+#endif
 		}
 #if NET6_0_OR_GREATER
 		else if (Value is TimeOnly timeOnlyValue)
 		{
+#if NET8_0_OR_GREATER
+			Utf8.TryWrite(writer.GetSpan(22), CultureInfo.InvariantCulture, $"time '{timeOnlyValue:HH':'mm':'ss'.'ffffff}'", out var bytesWritten);
+			writer.Advance(bytesWritten);
+#else
 			writer.WriteAscii(string.Create(CultureInfo.InvariantCulture, stackalloc char[22], $"time '{timeOnlyValue:HH':'mm':'ss'.'ffffff}'"));
+#endif
 		}
 #endif
 		else if (Value is TimeSpan ts)
@@ -395,12 +438,17 @@ public sealed class MySqlParameter : DbParameter, IDbDataParameter, ICloneable
 				writer.Write((byte) '-');
 				ts = TimeSpan.FromTicks(-ts.Ticks);
 			}
+#if NET8_0_OR_GREATER
+			Utf8.TryWrite(writer.GetSpan(17), CultureInfo.InvariantCulture, $"{ts.Days * 24 + ts.Hours}:{ts:mm':'ss'.'ffffff}'", out var bytesWritten);
+			writer.Advance(bytesWritten);
+#else
 #if NET6_0_OR_GREATER
 			var str = string.Create(CultureInfo.InvariantCulture, stackalloc char[17], $"{ts.Days * 24 + ts.Hours}:{ts:mm':'ss'.'ffffff}'");
 #else
 			var str = FormattableString.Invariant($"{ts.Days * 24 + ts.Hours}:{ts:mm':'ss'.'ffffff}'");
 #endif
 			writer.WriteAscii(str);
+#endif
 		}
 		else if (Value is Guid guidValue)
 		{
