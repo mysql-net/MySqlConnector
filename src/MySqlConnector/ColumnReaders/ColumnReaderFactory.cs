808f91fc42400b4bd88f75e3b7df6a9e590721d9
Bradley Grainger <bgrainger@gmail.com>
2023-06-16 22:38:38 -0700

    Eliminate repeated logic in ColumnReaderFactory.
    Signed-off-by: Bradley Grainger <bgrainger@gmail.com>

--- src/MySqlConnector/ColumnReaders/ColumnReaderFactory.cs
+++ src/MySqlConnector/ColumnReaders/ColumnReaderFactory.cs
@@ -6,200 +6,115 @@ namespace MySqlConnector.ColumnReaders;
 
 internal class ColumnReaderFactory
 {
-	internal static ColumnReader GetReader(bool binary, ColumnDefinitionPayload columnDefinition, MySqlConnection connection)
+	internal static ColumnReader GetReader(bool isBinary, ColumnDefinitionPayload columnDefinition, MySqlConnection connection)
 	{
 		var isUnsigned = (columnDefinition.ColumnFlags & ColumnFlags.Unsigned) != 0;
-		if (binary)
+		switch (columnDefinition.ColumnType)
 		{
-			switch (columnDefinition.ColumnType)
-			{
-				case ColumnType.Tiny:
-					if (connection.TreatTinyAsBoolean && columnDefinition.ColumnLength == 1 && !isUnsigned)
-						return BinaryBooleanColumnReader.Instance;
-					return isUnsigned ? BinaryUnsignedInt8ColumnReader.Instance : BinarySignedInt8ColumnReader.Instance;
+			case ColumnType.Tiny:
+				if (connection.TreatTinyAsBoolean && columnDefinition.ColumnLength == 1 && !isUnsigned)
+					return isBinary ? BinaryBooleanColumnReader.Instance : TextBooleanColumnReader.Instance;
+				return (isBinary, isUnsigned) switch
+				{
+					(false, false) => TextSignedInt8ColumnReader.Instance,
+					(false, true) => TextUnsignedInt8ColumnReader.Instance,
+					(true, false) => BinarySignedInt8ColumnReader.Instance,
+					(true, true) => BinaryUnsignedInt8ColumnReader.Instance,
+				};
 
-				case ColumnType.Int24:
-				case ColumnType.Long:
-					return isUnsigned ? BinaryUnsignedInt32ColumnReader.Instance : BinarySignedInt32ColumnReader.Instance;
+			case ColumnType.Int24:
+			case ColumnType.Long:
+				return (isBinary, isUnsigned) switch
+				{
+					(false, false) => TextSignedInt32ColumnReader.Instance,
+					(false, true) => TextUnsignedInt32ColumnReader.Instance,
+					(true, false) => BinarySignedInt32ColumnReader.Instance,
+					(true, true) => BinaryUnsignedInt32ColumnReader.Instance,
+				};
 
-				case ColumnType.Longlong:
-					return isUnsigned ? BinaryUnsignedInt64ColumnReader.Instance : BinarySignedInt64ColumnReader.Instance;
+			case ColumnType.Longlong:
+				return (isBinary, isUnsigned) switch
+				{
+					(false, false) => TextSignedInt64ColumnReader.Instance,
+					(false, true) => TextUnsignedInt64ColumnReader.Instance,
+					(true, false) => BinarySignedInt64ColumnReader.Instance,
+					(true, true) => BinaryUnsignedInt64ColumnReader.Instance,
+				};
 
-				case ColumnType.Bit:
-					return BitColumnReader.Instance;
+			case ColumnType.Bit:
+				return BitColumnReader.Instance;
 
-				case ColumnType.String:
-					if (connection.GuidFormat == MySqlGuidFormat.Char36
-					    && columnDefinition.ColumnLength / ProtocolUtility.GetBytesPerCharacter(columnDefinition.CharacterSet) == 36)
-						return GuidChar36ColumnReader.Instance;
-					if (connection.GuidFormat == MySqlGuidFormat.Char32
-					    && columnDefinition.ColumnLength / ProtocolUtility.GetBytesPerCharacter(columnDefinition.CharacterSet) == 32)
-						return GuidChar32ColumnReader.Instance;
-					goto case ColumnType.VarString;
+			case ColumnType.String:
+				if (connection.GuidFormat == MySqlGuidFormat.Char36 && columnDefinition.ColumnLength / ProtocolUtility.GetBytesPerCharacter(columnDefinition.CharacterSet) == 36)
+					return GuidChar36ColumnReader.Instance;
+				if (connection.GuidFormat == MySqlGuidFormat.Char32 && columnDefinition.ColumnLength / ProtocolUtility.GetBytesPerCharacter(columnDefinition.CharacterSet) == 32)
+					return GuidChar32ColumnReader.Instance;
+				goto case ColumnType.VarString;
 
-				case ColumnType.VarString:
-				case ColumnType.VarChar:
-				case ColumnType.TinyBlob:
-				case ColumnType.Blob:
-				case ColumnType.MediumBlob:
-				case ColumnType.LongBlob:
-				case ColumnType.Enum:
-				case ColumnType.Set:
-					if (columnDefinition.CharacterSet == CharacterSet.Binary)
+			case ColumnType.VarString:
+			case ColumnType.VarChar:
+			case ColumnType.TinyBlob:
+			case ColumnType.Blob:
+			case ColumnType.MediumBlob:
+			case ColumnType.LongBlob:
+			case ColumnType.Enum:
+			case ColumnType.Set:
+				if (columnDefinition.CharacterSet != CharacterSet.Binary)
+					return StringColumnReader.Instance;
+				if (columnDefinition.ColumnLength == 16)
+				{
+					return connection.GuidFormat switch
 					{
-						var guidFormat = connection.GuidFormat;
-						if ((guidFormat is MySqlGuidFormat.Binary16 or MySqlGuidFormat.TimeSwapBinary16
-							    or MySqlGuidFormat.LittleEndianBinary16) && columnDefinition.ColumnLength == 16)
-						{
-							switch (guidFormat)
-							{
-							case MySqlGuidFormat.Binary16:
-								return GuidBinary16ColumnReader.Instance;
-							case MySqlGuidFormat.TimeSwapBinary16:
-								return GuidTimeSwapBinary16ColumnReader.Instance;
-							default:
-								return GuidLittleEndianBinary16ColumnReader.Instance;
-							}
-						}
-						return BytesColumnReader.Instance;
-					}
-					return StringColumnReader.Instance;
+						MySqlGuidFormat.Binary16 => GuidBinary16ColumnReader.Instance,
+						MySqlGuidFormat.TimeSwapBinary16 => GuidTimeSwapBinary16ColumnReader.Instance,
+						MySqlGuidFormat.LittleEndianBinary16 => GuidLittleEndianBinary16ColumnReader.Instance,
+						_ => BytesColumnReader.Instance,
+					};
+				}
+				return BytesColumnReader.Instance;
 
-				case ColumnType.Json:
-					return StringColumnReader.Instance;
+			case ColumnType.Json:
+				return StringColumnReader.Instance;
 
-				case ColumnType.Short:
-					return isUnsigned ?
-						BinaryUnsignedInt16ColumnReader.Instance :
-						BinarySignedInt16ColumnReader.Instance;
+			case ColumnType.Short:
+				return (isBinary, isUnsigned) switch
+				{
+					(false, false) => TextSignedInt16ColumnReader.Instance,
+					(false, true) => TextUnsignedInt16ColumnReader.Instance,
+					(true, false) => BinarySignedInt16ColumnReader.Instance,
+					(true, true) => BinaryUnsignedInt16ColumnReader.Instance,
+				};
 
-				case ColumnType.Date:
-				case ColumnType.DateTime:
-				case ColumnType.NewDate:
-				case ColumnType.Timestamp:
-					return new BinaryDateTimeColumnReader(connection);
+			case ColumnType.Date:
+			case ColumnType.DateTime:
+			case ColumnType.NewDate:
+			case ColumnType.Timestamp:
+				return isBinary ? new BinaryDateTimeColumnReader(connection) : new TextDateTimeColumnReader(connection);
 
-				case ColumnType.Time:
-					return BinaryTimeColumnReader.Instance;
+			case ColumnType.Time:
+				return isBinary ? BinaryTimeColumnReader.Instance : TextTimeColumnReader.Instance;
 
-				case ColumnType.Year:
-					return BinaryYearColumnReader.Instance;
+			case ColumnType.Year:
+				return isBinary ? BinaryYearColumnReader.Instance : TextSignedInt32ColumnReader.Instance;
 
-				case ColumnType.Float:
-					return BinaryFloatColumnReader.Instance;
+			case ColumnType.Float:
+				return isBinary ? BinaryFloatColumnReader.Instance : TextFloatColumnReader.Instance;
 
-				case ColumnType.Double:
-					return BinaryDoubleColumnReader.Instance;
+			case ColumnType.Double:
+				return isBinary ? BinaryDoubleColumnReader.Instance : TextDoubleColumnReader.Instance;
 
-				case ColumnType.Decimal:
-				case ColumnType.NewDecimal:
-					return DecimalColumnReader.Instance;
+			case ColumnType.Decimal:
+			case ColumnType.NewDecimal:
+				return DecimalColumnReader.Instance;
 
-				case ColumnType.Geometry:
-					return BytesColumnReader.Instance;
+			case ColumnType.Geometry:
+				return BytesColumnReader.Instance;
 
-				case ColumnType.Null:
-					return NullColumnReader.Instance;
+			case ColumnType.Null:
+				return NullColumnReader.Instance;
 
-				default:
-					throw new NotImplementedException($"Reading {columnDefinition.ColumnType} not implemented");
-			}
-		}
-		else
-		{
-			switch (columnDefinition.ColumnType)
-			{
-				case ColumnType.Tiny:
-					if (connection.TreatTinyAsBoolean && columnDefinition.ColumnLength == 1 && !isUnsigned)
-						return TextBooleanColumnReader.Instance;
-					return isUnsigned ? TextUnsignedInt8ColumnReader.Instance : TextSignedInt8ColumnReader.Instance;
-
-				case ColumnType.Int24:
-				case ColumnType.Long:
-					return isUnsigned ? TextUnsignedInt32ColumnReader.Instance : TextSignedInt32ColumnReader.Instance;
-
-				case ColumnType.Longlong:
-					return isUnsigned ?
-						TextUnsignedInt64ColumnReader.Instance :
-						TextSignedInt64ColumnReader.Instance;
-
-				case ColumnType.Bit:
-					return BitColumnReader.Instance;
-
-				case ColumnType.String:
-					if (connection.GuidFormat == MySqlGuidFormat.Char36
-					    && columnDefinition.ColumnLength / ProtocolUtility.GetBytesPerCharacter(columnDefinition.CharacterSet) == 36)
-						return GuidChar36ColumnReader.Instance;
-					if (connection.GuidFormat == MySqlGuidFormat.Char32
-					    && columnDefinition.ColumnLength / ProtocolUtility.GetBytesPerCharacter(columnDefinition.CharacterSet) == 32)
-						return GuidChar32ColumnReader.Instance;
-					goto case ColumnType.VarString;
-
-				case ColumnType.VarString:
-				case ColumnType.VarChar:
-				case ColumnType.TinyBlob:
-				case ColumnType.Blob:
-				case ColumnType.MediumBlob:
-				case ColumnType.LongBlob:
-				case ColumnType.Enum:
-				case ColumnType.Set:
-					if (columnDefinition.CharacterSet == CharacterSet.Binary)
-					{
-						var guidFormat = connection.GuidFormat;
-						if ((guidFormat is MySqlGuidFormat.Binary16 or MySqlGuidFormat.TimeSwapBinary16
-							    or MySqlGuidFormat.LittleEndianBinary16) && columnDefinition.ColumnLength == 16)
-						{
-							switch (guidFormat)
-							{
-								case MySqlGuidFormat.Binary16:
-									return GuidBinary16ColumnReader.Instance;
-								case MySqlGuidFormat.TimeSwapBinary16:
-									return GuidTimeSwapBinary16ColumnReader.Instance;
-								default:
-									return GuidLittleEndianBinary16ColumnReader.Instance;
-							}
-						}
-						return BytesColumnReader.Instance;
-					}
-					return StringColumnReader.Instance;
-
-				case ColumnType.Json:
-					return StringColumnReader.Instance;
-
-				case ColumnType.Short:
-					return isUnsigned ? TextUnsignedInt16ColumnReader.Instance : TextSignedInt16ColumnReader.Instance;
-
-				case ColumnType.Date:
-				case ColumnType.DateTime:
-				case ColumnType.NewDate:
-				case ColumnType.Timestamp:
-					return new TextDateTimeColumnReader(connection);
-
-				case ColumnType.Time:
-					return TextTimeColumnReader.Instance;
-
-				case ColumnType.Float:
-					return TextFloatColumnReader.Instance;
-
-				case ColumnType.Double:
-					return TextDoubleColumnReader.Instance;
-
-				case ColumnType.Decimal:
-				case ColumnType.NewDecimal:
-					return DecimalColumnReader.Instance;
-
-				case ColumnType.Year:
-					return TextSignedInt32ColumnReader.Instance;
-
-				case ColumnType.Geometry:
-					return BytesColumnReader.Instance;
-
-				case ColumnType.Null:
-					return NullColumnReader.Instance;
-
-				default:
-					throw new NotImplementedException($"Reading {columnDefinition.ColumnType} not implemented");
-			}
+			default:
+				throw new NotImplementedException($"Reading {columnDefinition.ColumnType} not implemented");
 		}
 	}
 }
