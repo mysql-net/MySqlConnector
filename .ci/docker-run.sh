af4dc5a9e6fa4bee0bac29a20221782d3dcaac17
Bradley Grainger <bgrainger@gmail.com>
2023-06-25 14:05:38 -0700

    Test MariaDB 11.0.
    MariaDB 11.0 replaces 10.10 as the current STS version; it was released on 6 June 2023.

--- .ci/docker-run.sh
+++ .ci/docker-run.sh
@@ -23,12 +23,16 @@ IMAGE=$1
 PORT=$2
 OMIT_FEATURES=$3
 MYSQL_EXTRA=
+MYSQL=mysql
 
 if [ "$IMAGE" == "mysql:8.0" ]; then
   MYSQL_EXTRA='--default-authentication-plugin=mysql_native_password'
 fi
 if [[ "$IMAGE" == mariadb* ]]; then
   MYSQL_EXTRA='--in-predicate-conversion-threshold=100000'
+fi
+if [ "$IMAGE" == "mariadb:11.0" ]; then
+  MYSQL='mariadb'
 fi
 
 sudo mkdir -p run/mysql
@@ -52,40 +56,40 @@ for i in `seq 1 120`; do
 	# wait for mysql to come up
 	sleep 1
 	echo "Testing if container is responding"
-	docker exec mysql mysql -uroot -ptest -e "SELECT 1" >/dev/null 2>&1
+	docker exec mysql $MYSQL -uroot -ptest -e "SELECT 1" >/dev/null 2>&1
 	if [ $? -ne 0 ]; then continue; fi
 
 	# try running the init script
 	echo "Creating mysqltest user"
-	docker exec mysql bash -c 'mysql -uroot -ptest < /etc/mysql/conf.d/init.sql'
+	docker exec mysql bash -c "$MYSQL -uroot -ptest < /etc/mysql/conf.d/init.sql"
 	if [ $? -ne 0 ]; then continue; fi
 
 	if [[ $OMIT_FEATURES != *"Sha256Password"* ]]; then
 		echo "Creating sha256_password user"
-	 	docker exec mysql bash -c 'mysql -uroot -ptest < /etc/mysql/conf.d/init_sha256.sql'
+	 	docker exec mysql bash -c "$MYSQL -uroot -ptest < /etc/mysql/conf.d/init_sha256.sql"
 		if [ $? -ne 0 ]; then exit $?; fi
 	fi
 
 	if [[ $OMIT_FEATURES != *"CachingSha2Password"* ]]; then
 		echo "Creating caching_sha2_password user"
-		docker exec mysql bash -c 'mysql -uroot -ptest < /etc/mysql/conf.d/init_caching_sha2.sql'
+		docker exec mysql bash -c "$MYSQL -uroot -ptest < /etc/mysql/conf.d/init_caching_sha2.sql"
 		if [ $? -ne 0 ]; then exit $?; fi
 	fi
 
 	if [[ $OMIT_FEATURES != *"Ed25519"* ]]; then
 		echo "Installing auth_ed25519 component"
-		docker exec mysql bash -c 'mysql -uroot -ptest < /etc/mysql/conf.d/init_ed25519.sql'
+		docker exec mysql bash -c "$MYSQL -uroot -ptest < /etc/mysql/conf.d/init_ed25519.sql"
 		if [ $? -ne 0 ]; then exit $?; fi
 	fi
 
 	if [[ $OMIT_FEATURES != *"QueryAttributes"* ]]; then
 		echo "Installing query_attributes component"
-		docker exec mysql mysql -uroot -ptest -e "INSTALL COMPONENT 'file://component_query_attributes';"
+		docker exec mysql $MYSQL -uroot -ptest -e "INSTALL COMPONENT 'file://component_query_attributes';"
 		if [ $? -ne 0 ]; then exit $?; fi
 	fi
 
 	# exit if successful
-	docker exec mysql mysql -ussltest -ptest \
+	docker exec mysql $MYSQL -ussltest -ptest \
 		--ssl-mode=REQUIRED \
 		--ssl-ca=/etc/mysql/conf.d/certs/ssl-ca-cert.pem \
 		--ssl-cert=/etc/mysql/conf.d/certs/ssl-client-cert.pem \
@@ -93,7 +97,7 @@ for i in `seq 1 120`; do
 		-e "SELECT 1"
 	if [ $? -ne 0 ]; then
 		# mariadb uses --ssl=TRUE instead of --ssl-mode=REQUIRED
-		docker exec mysql mysql -ussltest -ptest \
+		docker exec mysql $MYSQL -ussltest -ptest \
 			--ssl=TRUE \
 			--ssl-ca=/etc/mysql/conf.d/certs/ssl-ca-cert.pem \
 			--ssl-cert=/etc/mysql/conf.d/certs/ssl-client-cert.pem \
