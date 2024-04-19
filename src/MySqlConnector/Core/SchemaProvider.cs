mysql-net/MySqlConnector

commit 9992b5def49e2c47ee29506a715cae180d68e646

Add `Foreign Keys`, `Indexes`, `IndexColumns` schema collections (#1419)
Signed-off-by: Evgeni Alesinskyy <ealesinskyy@exocad.com>
Signed-off-by: Bradley Grainger <bgrainger@gmail.com>
Co-authored-by: Evgeni Alesinskyy <ealesinskyy@exocad.com>

src/MySqlConnector/Core/SchemaProvider.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Text;
 using MySqlConnector.Protocol.Serialization;
 
 namespace MySqlConnector.Core;
@@ -383,6 +384,22 @@ internal sealed partial class SchemaProvider(MySqlConnection connection)
 
 	private async Task FillDataTableAsync(IOBehavior ioBehavior, DataTable dataTable, string tableName, List<KeyValuePair<string, string>>? columns, CancellationToken cancellationToken)
 	{
+		await FillDataTableAsync(ioBehavior, dataTable, command =>
+		{
+#pragma warning disable CA2100
+			command.CommandText = "SELECT " + string.Join(", ", dataTable.Columns.Cast<DataColumn>().Select(static x => x!.ColumnName)) + " FROM INFORMATION_SCHEMA." + tableName;
+#pragma warning restore CA2100
+			if (columns is { Count: > 0 })
+			{
+				command.CommandText += " WHERE " + string.Join(" AND ", columns.Select(x => $@"{x.Key} = @{x.Key}"));
+				foreach (var column in columns)
+					command.Parameters.AddWithValue("@" + column.Key, column.Value);
+			}
+		}, cancellationToken).ConfigureAwait(false);
+	}
+
+	private async Task FillDataTableAsync(IOBehavior ioBehavior, DataTable dataTable, Action<MySqlCommand> configureCommand, CancellationToken cancellationToken)
+	{
 		Action? close = null;
 		if (connection.State != ConnectionState.Open)
 		{
@@ -408,15 +425,7 @@ internal sealed partial class SchemaProvider(MySqlConnection connection)
 
 		using (var command = connection.CreateCommand())
 		{
-#pragma warning disable CA2100
-			command.CommandText = "SELECT " + string.Join(", ", dataTable.Columns.Cast<DataColumn>().Select(static x => x!.ColumnName)) + " FROM INFORMATION_SCHEMA." + tableName;
-#pragma warning restore CA2100
-			if (columns is { Count: > 0 })
-			{
-				command.CommandText += " WHERE " + string.Join(" AND ", columns.Select(x => $@"{x.Key} = @{x.Key}"));
-				foreach (var column in columns)
-					command.Parameters.AddWithValue("@" + column.Key, column.Value);
-			}
+			configureCommand(command);
 
 			using var reader = await command.ExecuteReaderAsync(default, ioBehavior, cancellationToken).ConfigureAwait(false);
 			while (await reader.ReadAsync(ioBehavior, cancellationToken).ConfigureAwait(false))
@@ -429,4 +438,109 @@ internal sealed partial class SchemaProvider(MySqlConnection connection)
 
 		close?.Invoke();
 	}
+
+	private Task DoFillForeignKeysAsync(IOBehavior ioBehavior, DataTable dataTable, string?[]? restrictionValues, CancellationToken cancellationToken) =>
+		FillDataTableAsync(IOBehavior.Synchronous, dataTable, command =>
+		{
+			command.CommandText = """
+				SELECT rc.constraint_catalog, rc.constraint_schema,
+					rc.constraint_name, kcu.table_catalog, kcu.table_schema, rc.table_name,
+					rc.match_option, rc.update_rule, rc.delete_rule, 
+					NULL as referenced_table_catalog,
+					kcu.referenced_table_schema, rc.referenced_table_name 
+				FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc
+					LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu ON 
+					kcu.constraint_catalog <=> rc.constraint_catalog AND
+					kcu.constraint_schema <=> rc.constraint_schema AND 
+					kcu.constraint_name <=> rc.constraint_name 
+				WHERE 1=1 AND kcu.ORDINAL_POSITION=1
+				""";
+
+			if (restrictionValues is not null)
+			{
+				var where = "";
+				if (restrictionValues.Length >= 2 && !string.IsNullOrEmpty(restrictionValues[1]))
+				{
+					where += " AND rc.constraint_schema LIKE @schema";
+					command.Parameters.AddWithValue("@schema", restrictionValues[1]);
+				}
+				if (restrictionValues.Length >= 3 && !string.IsNullOrEmpty(restrictionValues[2]))
+				{
+					where += " AND rc.table_name LIKE @table";
+					command.Parameters.AddWithValue("@table", restrictionValues[2]);
+				}
+				if (restrictionValues.Length >= 4 && !string.IsNullOrEmpty(restrictionValues[3]))
+				{
+					where += " AND rc.constraint_name LIKE @constraint";
+					command.Parameters.AddWithValue("@constraint", restrictionValues[3]);
+				}
+				command.CommandText += where;
+			}
+		}, cancellationToken);
+
+	private Task DoFillIndexesAsync(IOBehavior ioBehavior, DataTable dataTable, string?[]? restrictionValues, CancellationToken cancellationToken) =>
+		FillDataTableAsync(ioBehavior, dataTable, command =>
+		{
+			command.CommandText = """
+				SELECT DISTINCT null AS INDEX_CATALOG, INDEX_SCHEMA,
+					INDEX_NAME, TABLE_NAME,
+					!NON_UNIQUE as `UNIQUE`, 
+					INDEX_NAME='PRIMARY' as `PRIMARY`,
+					INDEX_TYPE as TYPE, COMMENT 
+				FROM INFORMATION_SCHEMA.STATISTICS
+				WHERE 1=1
+				""";
+
+			if (restrictionValues is not null)
+			{
+				var where = "";
+				if (restrictionValues.Length >= 2 && !string.IsNullOrEmpty(restrictionValues[1]))
+				{
+					where += " AND INDEX_SCHEMA LIKE @schema";
+					command.Parameters.AddWithValue("@schema", restrictionValues[1]);
+				}
+				if (restrictionValues.Length >= 3 && !string.IsNullOrEmpty(restrictionValues[2]))
+				{
+					where += " AND TABLE_NAME LIKE @table";
+					command.Parameters.AddWithValue("@table", restrictionValues[2]);
+				}
+
+				command.CommandText += where;
+			}
+		}, cancellationToken);
+
+	private Task DoFillIndexColumnsAsync(IOBehavior ioBehavior, DataTable dataTable, string?[]? restrictionValues, CancellationToken cancellationToken) =>
+		FillDataTableAsync(ioBehavior, dataTable, command =>
+		{
+			command.CommandText = """
+				SELECT null AS INDEX_CATALOG, INDEX_SCHEMA,
+					INDEX_NAME, TABLE_NAME,
+					COLUMN_NAME,
+					SEQ_IN_INDEX as `ORDINAL_POSITION`,
+					COLLATION as SORT_ORDER
+				FROM INFORMATION_SCHEMA.STATISTICS
+				WHERE 1=1
+				""";
+
+			if (restrictionValues is not null)
+			{
+				var where = "";
+				if (restrictionValues.Length >= 2 && !string.IsNullOrEmpty(restrictionValues[1]))
+				{
+					where += " AND INDEX_SCHEMA LIKE @schema";
+					command.Parameters.AddWithValue("@schema", restrictionValues[1]);
+				}
+				if (restrictionValues.Length >= 3 && !string.IsNullOrEmpty(restrictionValues[2]))
+				{
+					where += " AND TABLE_NAME LIKE @table";
+					command.Parameters.AddWithValue("@table", restrictionValues[2]);
+				}
+				if (restrictionValues.Length >= 4 && !string.IsNullOrEmpty(restrictionValues[3]))
+				{
+					where += " AND INDEX_NAME LIKE @index";
+					command.Parameters.AddWithValue("@index", restrictionValues[3]);
+				}
+				command.CommandText += where;
+			}
+		}, cancellationToken);
 }

src/MySqlConnector/Core/SchemaProvider.g.cs
@@ -75,6 +75,12 @@ internal sealed partial class SchemaProvider
 			await FillUserPrivilegesAsync(ioBehavior, dataTable, "UserPrivileges", restrictionValues, cancellationToken).ConfigureAwait(false);
 		else if (string.Equals(collectionName, "Views", StringComparison.OrdinalIgnoreCase))
 			await FillViewsAsync(ioBehavior, dataTable, "Views", restrictionValues, cancellationToken).ConfigureAwait(false);
+		else if (string.Equals(collectionName, "Foreign Keys", StringComparison.OrdinalIgnoreCase))
+			await FillForeignKeysAsync(ioBehavior, dataTable, "Foreign Keys", restrictionValues, cancellationToken).ConfigureAwait(false);
+		else if (string.Equals(collectionName, "Indexes", StringComparison.OrdinalIgnoreCase))
+			await FillIndexesAsync(ioBehavior, dataTable, "Indexes", restrictionValues, cancellationToken).ConfigureAwait(false);
+		else if (string.Equals(collectionName, "IndexColumns", StringComparison.OrdinalIgnoreCase))
+			await FillIndexColumnsAsync(ioBehavior, dataTable, "IndexColumns", restrictionValues, cancellationToken).ConfigureAwait(false);
 		else
 			throw new ArgumentException($"Invalid collection name: '{collectionName}'.", nameof(collectionName));
 
@@ -123,6 +129,9 @@ internal sealed partial class SchemaProvider
 		dataTable.Rows.Add("Triggers", 0, 3);
 		dataTable.Rows.Add("UserPrivileges", 0, 0);
 		dataTable.Rows.Add("Views", 0, 3);
+		dataTable.Rows.Add("Foreign Keys", 4, 0);
+		dataTable.Rows.Add("Indexes", 4, 0);
+		dataTable.Rows.Add("IndexColumns", 5, 0);
 
 		return Task.CompletedTask;
 	}
@@ -628,6 +637,19 @@ internal sealed partial class SchemaProvider
 		dataTable.Rows.Add("Tables", "Schema", "TABLE_SCHEMA", 2);
 		dataTable.Rows.Add("Tables", "Table", "TABLE_NAME", 3);
 		dataTable.Rows.Add("Tables", "TableType", "TABLE_TYPE", 4);
+		dataTable.Rows.Add("Foreign Keys", "Catalog", "TABLE_CATALOG", 1);
+		dataTable.Rows.Add("Foreign Keys", "Schema", "TABLE_SCHEMA", 2);
+		dataTable.Rows.Add("Foreign Keys", "Table", "TABLE_NAME", 3);
+		dataTable.Rows.Add("Foreign Keys", "Column", "COLUMN_NAME", 4);
+		dataTable.Rows.Add("Indexes", "Catalog", "TABLE_CATALOG", 1);
+		dataTable.Rows.Add("Indexes", "Schema", "TABLE_SCHEMA", 2);
+		dataTable.Rows.Add("Indexes", "Table", "TABLE_NAME", 3);
+		dataTable.Rows.Add("Indexes", "Column", "COLUMN_NAME", 4);
+		dataTable.Rows.Add("IndexColumns", "Catalog", "TABLE_CATALOG", 1);
+		dataTable.Rows.Add("IndexColumns", "Schema", "TABLE_SCHEMA", 2);
+		dataTable.Rows.Add("IndexColumns", "Table", "TABLE_NAME", 3);
+		dataTable.Rows.Add("IndexColumns", "Constraint", "CONSTRAINT_NAME", 4);
+		dataTable.Rows.Add("IndexColumns", "Column", "COLUMN_NAME", 5);
 
 		return Task.CompletedTask;
 	}
@@ -830,6 +852,72 @@ internal sealed partial class SchemaProvider
 		]);
 
 		await FillDataTableAsync(ioBehavior, dataTable, "VIEWS", null, cancellationToken).ConfigureAwait(false);
+	}
+
+	private async Task FillForeignKeysAsync(IOBehavior ioBehavior, DataTable dataTable, string tableName, string?[]? restrictionValues, CancellationToken cancellationToken)
+	{
+		if (restrictionValues is { Length: > 4 })
+			throw new ArgumentException("More than 4 restrictionValues are not supported for schema 'Foreign Keys'.", nameof(restrictionValues));
+
+		dataTable.TableName = tableName;
+		dataTable.Columns.AddRange(
+		[
+			new("CONSTRAINT_CATALOG", typeof(string)),
+			new("CONSTRAINT_SCHEMA", typeof(string)),
+			new("CONSTRAINT_NAME", typeof(string)),
+			new("TABLE_CATALOG", typeof(string)),
+			new("TABLE_SCHEMA", typeof(string)),
+			new("TABLE_NAME", typeof(string)),
+			new("MATCH_OPTION", typeof(string)),
+			new("UPDATE_RULE", typeof(string)),
+			new("DELETE_RULE", typeof(string)),
+			new("REFERENCED_TABLE_CATALOG", typeof(string)),
+			new("REFERENCED_TABLE_SCHEMA", typeof(string)),
+			new("REFERENCED_TABLE_NAME", typeof(string)),
+		]);
+
+		await DoFillForeignKeysAsync(ioBehavior, dataTable, restrictionValues, cancellationToken).ConfigureAwait(false);
+	}
+
+	private async Task FillIndexesAsync(IOBehavior ioBehavior, DataTable dataTable, string tableName, string?[]? restrictionValues, CancellationToken cancellationToken)
+	{
+		if (restrictionValues is { Length: > 4 })
+			throw new ArgumentException("More than 4 restrictionValues are not supported for schema 'Indexes'.", nameof(restrictionValues));
+
+		dataTable.TableName = tableName;
+		dataTable.Columns.AddRange(
+		[
+			new("INDEX_CATALOG", typeof(string)),
+			new("INDEX_SCHEMA", typeof(string)),
+			new("INDEX_NAME", typeof(string)),
+			new("TABLE_NAME", typeof(string)),
+			new("UNIQUE", typeof(bool)),
+			new("PRIMARY", typeof(bool)),
+			new("TYPE", typeof(string)),
+			new("COMMENT", typeof(string)),
+		]);
+
+		await DoFillIndexesAsync(ioBehavior, dataTable, restrictionValues, cancellationToken).ConfigureAwait(false);
+	}
+
+	private async Task FillIndexColumnsAsync(IOBehavior ioBehavior, DataTable dataTable, string tableName, string?[]? restrictionValues, CancellationToken cancellationToken)
+	{
+		if (restrictionValues is { Length: > 5 })
+			throw new ArgumentException("More than 5 restrictionValues are not supported for schema 'IndexColumns'.", nameof(restrictionValues));
+
+		dataTable.TableName = tableName;
+		dataTable.Columns.AddRange(
+		[
+			new("INDEX_CATALOG", typeof(string)),
+			new("INDEX_SCHEMA", typeof(string)),
+			new("INDEX_NAME", typeof(string)),
+			new("TABLE_NAME", typeof(string)),
+			new("COLUMN_NAME", typeof(string)),
+			new("ORDINAL_POSITION", typeof(int)),
+			new("SORT_ORDER", typeof(string)),
+		]);
+
+		await DoFillIndexColumnsAsync(ioBehavior, dataTable, restrictionValues, cancellationToken).ConfigureAwait(false);
 	}
 
 }

tests/IntegrationTests/SchemaProviderFixture.cs
@@ -1,0 +1,32 @@
+namespace IntegrationTests;
+
+public class SchemaProviderFixture : DatabaseFixture
+{
+	public SchemaProviderFixture()
+	{
+		Connection.Execute("""
+			DROP TABLE IF EXISTS fk_test;
+			DROP TABLE IF EXISTS pk_test;
+
+			CREATE TABLE pk_test
+			(
+				a INT NOT NULL,
+				b INT NOT NULL,
+				c INT NOT NULL,
+				d INT NOT NULL,
+				e INT NOT NULL,
+				CONSTRAINT pk_test_pk PRIMARY KEY (a, b),
+				CONSTRAINT pk_test_uq UNIQUE INDEX (c, d, e),
+				INDEX pk_test_ix (c, d)
+			);
+
+			CREATE TABLE fk_test
+			(
+				g INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+				h INT NOT NULL,
+				i INT NOT NULL,
+				CONSTRAINT fk_test_fk FOREIGN KEY (h, i) REFERENCES pk_test(a, b)
+			);
+			""");
+	}
+}

tests/IntegrationTests/SchemaProviderTests.cs
@@ -1,8 +1,9 @@
+#nullable enable
 namespace IntegrationTests;
 
-public class SchemaProviderTests : IClassFixture<DatabaseFixture>, IDisposable
+public class SchemaProviderTests : IClassFixture<SchemaProviderFixture>, IDisposable
 {
-	public SchemaProviderTests(DatabaseFixture database)
+	public SchemaProviderTests(SchemaProviderFixture database)
 	{
 		m_database = database;
 		m_database.Connection.Open();
@@ -118,6 +119,8 @@ public class SchemaProviderTests : IClassFixture<DatabaseFixture>, IDisposable
 	[InlineData("DATABASES", "Databases")]
 	[InlineData("DataTypes")]
 	[InlineData("datatypes", "DataTypes")]
+	[InlineData("Indexes")]
+	[InlineData("IndexColumns")]
 	// only in 8.0 - [InlineData("KeyWords")]
 	[InlineData("MetaDataCollections")]
 	[InlineData("Procedures")]
@@ -130,6 +133,7 @@ public class SchemaProviderTests : IClassFixture<DatabaseFixture>, IDisposable
 	[InlineData("CollationCharacterSetApplicability")]
 	[InlineData("Collations")]
 	[InlineData("Engines")]
+	[InlineData("Foreign Keys")]
 	[InlineData("KeyColumnUsage")]
 	[InlineData("Parameters")]
 	[InlineData("Partitions")]
@@ -143,7 +147,7 @@ public class SchemaProviderTests : IClassFixture<DatabaseFixture>, IDisposable
 	[InlineData("TableSpaces")]
 	[InlineData("UserPrivileges")]
 #endif
-	public void GetSchema(string schemaName, string expectedSchemaName = null)
+	public void GetSchema(string schemaName, string? expectedSchemaName = null)
 	{
 		var table = m_database.Connection.GetSchema(schemaName);
 		Assert.NotNull(table);
@@ -176,6 +180,67 @@ public class SchemaProviderTests : IClassFixture<DatabaseFixture>, IDisposable
 		Assert.Contains("ascii_bin", table.Rows.Cast<DataRow>().Select(x => (string) x[0]));
 	}
 #endif
+
+	[Fact]
+	public void ForeignKeys()
+    {
+        var schemaName = m_database.Connection.Database;
+        var table = m_database.Connection.GetSchema("Foreign Keys", new[] { null, schemaName, "fk_test" });
+        var row = table.Rows.Cast<DataRow>().Single();
+        foreach (var (column, value) in new[]
+        {
+			("CONSTRAINT_CATALOG", "def"),
+			("CONSTRAINT_SCHEMA", schemaName),
+			("CONSTRAINT_NAME", "fk_test_fk"),
+			("TABLE_CATALOG", "def"),
+			("TABLE_SCHEMA", schemaName),
+			("TABLE_NAME", "fk_test"),
+			("MATCH_OPTION", "NONE"),
+			("REFERENCED_TABLE_CATALOG", null),
+			("REFERENCED_TABLE_SCHEMA", schemaName),
+			("REFERENCED_TABLE_NAME", "pk_test"),
+		})
+        {
+			Assert.Equal(value, row[column] is DBNull ? null : (string?) row[column]);
+        }
+    }
+
+	[Fact]
+	public void Indexes()
+	{
+		var schemaName = m_database.Connection.Database;
+		var table = m_database.Connection.GetSchema("Indexes", new[] { null, schemaName, "pk_test" });
+		var actual = table.Rows
+			.Cast<DataRow>()
+			.OrderBy(x => (string) x["INDEX_NAME"])
+			.Select(x => ((string) x["INDEX_SCHEMA"], (string) x["INDEX_NAME"], (string) x["TABLE_NAME"], (bool) x["UNIQUE"], (bool) x["PRIMARY"], (string) x["TYPE"]));
+		var expected = new[]
+		{
+			(schemaName, "pk_test_ix", "pk_test", false, false, "BTREE"),
+			(schemaName, "pk_test_uq", "pk_test", true, false, "BTREE"),
+			(schemaName, "PRIMARY", "pk_test", true, true, "BTREE"),
+		};
+		Assert.Equal(expected, actual);
+	}
+
+	[Fact]
+	public void IndexColumns()
+	{
+		var schemaName = m_database.Connection.Database;
+		var table = m_database.Connection.GetSchema("IndexColumns", new[] { null, schemaName, "pk_test", "pk_test_uq" });
+		var actual = table.Rows
+			.Cast<DataRow>()
+			.OrderBy(x => (string) x["INDEX_NAME"])
+			.ThenBy(x => (int) x["ORDINAL_POSITION"])
+			.Select(x => ((string) x["INDEX_SCHEMA"], (string) x["INDEX_NAME"], (string) x["TABLE_NAME"], (string) x["COLUMN_NAME"], (int) x["ORDINAL_POSITION"]));
+		var expected = new[]
+		{
+			(schemaName, "pk_test_uq", "pk_test", "c", 1),
+			(schemaName, "pk_test_uq", "pk_test", "d", 2),
+			(schemaName, "pk_test_uq", "pk_test", "e", 3),
+		};
+		Assert.Equal(expected, actual);
+	}
 
 	readonly DatabaseFixture m_database;
 }

tools/SchemaCollectionGenerator/SchemaCollectionGenerator.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
 
@@ -11,7 +12,7 @@ var deserializer = new DeserializerBuilder()
 	.Build();
 var schemaCollections = deserializer.Deserialize<List<Schema>>(reader);
 
-using var codeWriter = new StreamWriter(@"..\..\..\..\..\src\MySqlConnector\Core\SchemaProvider.g.cs");
+using var codeWriter = new StreamWriter(@"..\..\src\MySqlConnector\Core\SchemaProvider.g.cs");
 codeWriter.WriteLine("""
 	// DO NOT EDIT - generated by SchemaCollectionGenerator.cs
 	#nullable enable
@@ -36,9 +37,10 @@ codeWriter.WriteLine("""
 var elseIf = "if";
 foreach (var schema in schemaCollections)
 {
+	string methodSchemaName = Regexes.NonAsciiAlphaNumeric().Replace(schema.Name, "");
 	codeWriter.WriteLine($"""
 				{elseIf} (string.Equals(collectionName, "{schema.Name}", StringComparison.OrdinalIgnoreCase))
-					await Fill{schema.Name}Async(ioBehavior, dataTable, "{schema.Name}", restrictionValues, cancellationToken).ConfigureAwait(false);
+					await Fill{methodSchemaName}Async(ioBehavior, dataTable, "{schema.Name}", restrictionValues, cancellationToken).ConfigureAwait(false);
 		""");
 	elseIf = "else if";
 }
@@ -53,10 +55,11 @@ codeWriter.WriteLine("""
 
 foreach (var schema in schemaCollections)
 {
-	var isAsync = schema.Table is not null;
+	var isAsync = schema.Table is not null || schema.Custom?.EndsWith("Async") is true;
+	string methodSchemaName = Regexes.NonAsciiAlphaNumeric().Replace(schema.Name, "");
 	var supportsRestrictions = schema.Restrictions is { Count: > 0 };
 	codeWriter.WriteLine($$"""
-			private {{(isAsync ? "async " : "")}}Task Fill{{schema.Name}}Async(IOBehavior ioBehavior, DataTable dataTable, string tableName, string?[]? restrictionValues, CancellationToken cancellationToken)
+			private {{(isAsync ? "async " : "")}}Task Fill{{methodSchemaName}}Async(IOBehavior ioBehavior, DataTable dataTable, string tableName, string?[]? restrictionValues, CancellationToken cancellationToken)
 			{
 		""");
 	if (!supportsRestrictions)
@@ -145,9 +148,13 @@ foreach (var schema in schemaCollections)
 	}
 	else
 	{
-		codeWriter.WriteLine($"""
-					{schema.Custom}(dataTable);
+		// custom function will be called here
+		string restrictions = (schema.Restrictions?.Count > 0) ? ", restrictionValues" : "";
+		codeWriter.Write($"""
+					{(isAsync ? "await " : "")}{schema.Custom}({(isAsync ? "ioBehavior, " : "")}dataTable{restrictions}{(isAsync ? ", cancellationToken" : "")}){(isAsync ? ".ConfigureAwait(false)" : "")};
 			""");
+		if (!isAsync)
+			codeWriter.WriteLine();
 	}
 
 	if (!isAsync)
@@ -169,7 +176,7 @@ codeWriter.WriteLine("""
 	}
 	""");
 
-using var docWriter = new StreamWriter(@"..\..\..\..\..\docs\content\overview\schema-collections.md");
+using var docWriter = new StreamWriter(@"..\..\docs\content\overview\schema-collections.md");
 docWriter.Write($"""
 	---
 	date: 2021-04-24
@@ -197,7 +204,7 @@ foreach (var schema in schemaCollections)
 
 foreach (var schema in schemaCollections.Where(x => x.Description is not null))
 {
-	using var schemaDocWriter = new StreamWriter($@"..\..\..\..\..\docs\content\overview\schema\{schema.Name.ToLowerInvariant()}.md");
+	using var schemaDocWriter = new StreamWriter($@"..\..\docs\content\overview\schema\{schema.Name.ToLowerInvariant()}.md");
 #pragma warning restore CA1308 // Normalize strings to uppercase
 	schemaDocWriter.Write($"""
 		---
@@ -257,4 +264,10 @@ internal sealed class Restriction
 {
 	public required string Name { get; init; }
 	public required string Default { get; init; }
+}
+
+internal sealed partial class Regexes
+{
+	[GeneratedRegex("[^a-z0-9]", RegexOptions.IgnoreCase | RegexOptions.Compiled, "en-US")]
+	public static partial Regex NonAsciiAlphaNumeric();
 }

tools/SchemaCollectionGenerator/SchemaCollections.yaml
@@ -759,3 +759,101 @@
     type: string
   - name: COLLATION_CONNECTION
     type: string
+
+- name: Foreign Keys
+  description: information about foreign keys in the server's SQL syntax
+  custom: DoFillForeignKeysAsync
+  restrictions:
+  - name: Catalog
+    default: TABLE_CATALOG
+  - name: Schema
+    default: TABLE_SCHEMA
+  - name: Table
+    default: TABLE_NAME
+  - name: Column
+    default: COLUMN_NAME
+  columns:
+  - name: CONSTRAINT_CATALOG
+    type: string
+  - name: CONSTRAINT_SCHEMA
+    type: string
+  - name: CONSTRAINT_NAME
+    type: string
+  - name: TABLE_CATALOG
+    type: string
+  - name: TABLE_SCHEMA
+    type: string
+  - name: TABLE_NAME
+    type: string
+  - name: MATCH_OPTION
+    type: string
+  - name: UPDATE_RULE
+    type: string
+  - name: DELETE_RULE
+    type: string
+  - name: REFERENCED_TABLE_CATALOG
+    type: string
+  - name: REFERENCED_TABLE_SCHEMA
+    type: string
+  - name: REFERENCED_TABLE_NAME
+    type: string
+
+- name: Indexes
+  description: information about indexes in the server's SQL syntax
+  custom: DoFillIndexesAsync
+  restrictions:
+  - name: Catalog
+    default: TABLE_CATALOG
+  - name: Schema
+    default: TABLE_SCHEMA
+  - name: Table
+    default: TABLE_NAME
+  - name: Column # it is only a guess - might need to be changed, but NHibernate passes 4 values, with last one is null
+    default: COLUMN_NAME
+  columns:
+  - name: INDEX_CATALOG # placeholder  - will be filled by SQL query
+    type: string
+  - name: INDEX_SCHEMA # same as TABLE_SCHEMA, see https://mariadb.com/kb/en/information-schema-statistics-table/
+    type: string
+  - name: INDEX_NAME
+    type: string
+  - name: TABLE_NAME
+    type: string
+  - name: UNIQUE
+    type: bool
+  - name: PRIMARY
+    type: bool
+  - name: TYPE
+    type: string
+  - name: COMMENT
+    type: string
+
+- name: IndexColumns
+  description: information about indexes in the server's SQL syntax
+  custom: DoFillIndexColumnsAsync
+  restrictions:
+  - name: Catalog
+    default: TABLE_CATALOG
+  - name: Schema
+    default: TABLE_SCHEMA
+  - name: Table
+    default: TABLE_NAME
+  - name: Constraint
+    default: CONSTRAINT_NAME
+  - name: Column # it comes fro
+    default: COLUMN_NAME
+  columns:
+  - name: INDEX_CATALOG # placeholder  - will be filled by SQL query
+    type: string
+  - name: INDEX_SCHEMA # same as TABLE_SCHEMA, see https://mariadb.com/kb/en/information-schema-statistics-table/
+    type: string
+  - name: INDEX_NAME
+    type: string
+  - name: TABLE_NAME
+    type: string
+  - name: COLUMN_NAME
+    type: string
+  - name: ORDINAL_POSITION
+    type: int
+  - name: SORT_ORDER
+    type: string

commit a7987abb4af4df4b58465aa8fafb293e89553a88

Fix bug preventing asynchronous creation of "Foreign Keys" schema.
This fixes an error introduced in https://github.com/mysql-net/MySqlConnector/pull/1419.

src/MySqlConnector/Core/SchemaProvider.cs
@@ -441,7 +441,7 @@ internal sealed partial class SchemaProvider(MySqlConnection connection)
 	}
 
 	private Task DoFillForeignKeysAsync(IOBehavior ioBehavior, DataTable dataTable, string?[]? restrictionValues, CancellationToken cancellationToken) =>
-		FillDataTableAsync(IOBehavior.Synchronous, dataTable, command =>
+		FillDataTableAsync(ioBehavior, dataTable, command =>
 		{
 			command.CommandText = """
 				SELECT rc.constraint_catalog, rc.constraint_schema, rc.constraint_name,