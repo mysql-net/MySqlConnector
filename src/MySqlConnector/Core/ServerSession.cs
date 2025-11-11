fb0ce27d326eadd06af71628403d420c10e68acc
Bradley Grainger <bgrainger@gmail.com>
2025-11-11 09:45:02 -0800

    Handle extra byte in ext-salt. Fixes #1606

--- src/MySqlConnector/Core/ServerSession.cs
+++ src/MySqlConnector/Core/ServerSession.cs
@@ -943,10 +943,15 @@ internal sealed partial class ServerSession : IServerCapabilities
 				payload = new([]);
 				await SendReplyAsync(payload, ioBehavior, cancellationToken).ConfigureAwait(false);
 				payload = await ReceiveReplyAsync(ioBehavior, cancellationToken).ConfigureAwait(false);
+				var extendedSalt = payload.Span;
 
-				Span<byte> combinedData = stackalloc byte[switchRequest.Data.Length + payload.Span.Length];
+				// MariaDB 11.8.4 sends an extra 0x01 byte at the beginning of ext-salt; cf. https://mariadb.com/docs/server/reference/clientserver-protocol/1-connecting/connection#parsec-plugin
+				if (extendedSalt.Length == 21 && extendedSalt[0] == 1 && extendedSalt[1] == 'P')
+					extendedSalt = extendedSalt[1..];
+
+				Span<byte> combinedData = stackalloc byte[switchRequest.Data.Length + extendedSalt.Length];
 				switchRequest.Data.CopyTo(combinedData);
-				payload.Span.CopyTo(combinedData.Slice(switchRequest.Data.Length));
+				extendedSalt.CopyTo(combinedData.Slice(switchRequest.Data.Length));
 
 				parsecPlugin3.CreateResponseAndPasswordHash(password, combinedData, out var parsecResponse, out m_passwordHash);
 				payload = new(parsecResponse);
